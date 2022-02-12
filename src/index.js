bdc8e443c5f558984a40a2628d1edd44b86a8864
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-12 22:14:12 +0200

    re-organise files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './__tests__/reportWebVitals';
+import './assets/scss/core.scss';
 
 ReactDOM.render(
   <React.StrictMode>
