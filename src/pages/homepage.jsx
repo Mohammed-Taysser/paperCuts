8b5e7f6d125031e1b97dfd5eb4c0da3535e8dba5
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-14 07:47:30 +0200

    add Testimonials & latest books to homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Services from '../components/Services';
 import Header from '../components/layout/Header';
+import LatestBooks from '../components/LatestBooks';
+import Testimonials from '../components/Testimonials';
 
 function homepage() {
   return (
     <>
       <Header />
       <Services />
+      <LatestBooks />
+      <Testimonials />
     </>
   );
 }
