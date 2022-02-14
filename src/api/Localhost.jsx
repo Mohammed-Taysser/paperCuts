b7ec65da6557fcf9e9f0acad4178d02c89d784ac
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-14 17:45:01 +0200

    add related product api

--- src/api/Localhost.jsx
+++ src/api/Localhost.jsx
@@ -19,6 +19,8 @@ function shuffle_arr(arr) {
 
 const SERVICES = Data.services;
 const REVIEWS = Data.reviews;
+const BOOKS = Data.books;
 const LATEST_BOOKS = shuffle_arr(Data.books.slice(0, 8));
+const RELATED_BOOKS = shuffle_arr(Data.books.slice(0, 4));
 
-export { SERVICES, REVIEWS, LATEST_BOOKS };
+export { SERVICES, REVIEWS, BOOKS, LATEST_BOOKS, RELATED_BOOKS };
