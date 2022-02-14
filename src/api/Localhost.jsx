32665d547c95ce9baf44b7b040ec8ed01d42853b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-14 07:51:02 +0200

    temporary use static data

--- src/api/Localhost.jsx
+++ src/api/Localhost.jsx
@@ -1,12 +1,24 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const base_url = axios.create({
-  baseURL: 'http://localhost:8080/db',
-});
+// const base_url = axios.create({
+//   baseURL: 'http://localhost:8080/db',
+// });
 
-const ServicesAPI = axios.create({
-  baseURL: 'http://localhost:8080/services',
-});
+// const ServicesAPI = axios.create({
+//   baseURL: 'http://localhost:8080/services',
+// });
 
-export default base_url;
-export { base_url, ServicesAPI };
+// export default base_url;
+// export { base_url, ServicesAPI };
+
+import Data from '../assets/json/db.json';
+
+function shuffle_arr(arr) {
+  return arr.sort(() => 0.5 - Math.random());
+}
+
+const SERVICES = Data.services;
+const REVIEWS = Data.reviews;
+const LATEST_BOOKS = shuffle_arr(Data.books.slice(0, 8));
+
+export { SERVICES, REVIEWS, LATEST_BOOKS };
