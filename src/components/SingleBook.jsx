6f8a7ca925151f0306ec7f412187ec2fc7df5d73
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-15 20:01:57 +0200

    add col-media_query

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -6,14 +6,16 @@ function SingleBook(props) {
   const { book } = props;
 
   return (
-    <div className='col-sm-6 col-lg-3 my-3'>
+    <div className={`col-sm-6 col-lg-${props.col} my-3`}>
       <div className='card border-0 nice-shadow h-100 single-book'>
         <div className='img'>
           <img src={book.img} className='card-img-top' alt={book.title} />
         </div>
         <div className='card-body'>
           <h5 className='card-title'>
-            <Link to={`/books/${book.id}`} className='stretched-link'> {book.title} </Link>
+            <Link to={`/books/${book.id}`} className='stretched-link'>
+              {book.title}
+            </Link>
           </h5>
           <h6 className='card-subtitle mb-3 text-muted'> {book.author} </h6>
           <h6 className='card-subtitle d-flex justify-content-between'>
@@ -25,5 +27,9 @@ function SingleBook(props) {
     </div>
   );
 }
+
+SingleBook.defaultProps = {
+  col: 3,
+};
 
 export default SingleBook;
