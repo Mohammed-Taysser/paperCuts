8524d9b4ca5af7b919ab0eec04fbc912b667a95a
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-15 13:37:05 +0200

    add routes for only auth users

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import AuthContext from '../context/auth';
@@ -8,6 +8,7 @@ import PageNotFound from '../pages/404';
 import AboutUs from '../pages/about-us';
 import Login from '../pages/login';
 import Register from '../pages/register';
+import ForgetPassword from '../pages/forget-password';
 import Profile from '../pages/profile';
 import Books from '../pages/books';
 import Search from '../pages/search';
@@ -17,6 +18,16 @@ import CategoryDetails from '../pages/categoryDetails';
 
 function PaperCutsRoutes() {
   const auth_context = useContext(AuthContext);
+  const auth_routes = () => {
+    if (auth_context.isAuth) {
+      return (
+        <>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/forget-password' element={<ForgetPassword />} />
+        </>
+      );
+    }
+  };
   return (
     <>
       <Routes>
@@ -26,11 +37,7 @@ function PaperCutsRoutes() {
         <Route path='/search' element={<Search />} />
         <Route path='/register' element={<Register />} />
         <Route path='/about-us' element={<AboutUs />} />
-        {
-          auth_context.isAuth 
-            ?  <Route path='/profile' element={<Profile />} />
-            : null 
-        }
+        {auth_routes()}
         <Route path='/books' element={<Books />} />
         <Route path='/books/:id' element={<BooksDetails />} />
         <Route path='/category' element={<Category />} />
