ec88a1fe3d95dbbaf2ed41c789364830b6ff6f60
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-15 12:27:12 +0200

    finish 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import PageNotFoundImage from '../assets/img/background/404-img.jpg';
 
 function PageNotFound() {
   return (
-    <div>404</div>
-  )
+    <section
+      className='page-not-found'
+      style={{ backgroundImage: `url(${PageNotFoundImage})` }}
+    >
+      <div className='wrapper'>
+        <h1 className='display-4 mb-4'>404</h1>
+        <h2> Page not found </h2>
+        <p className=''>
+          Oops! The page you are looking for does not exist. It might have been
+          moved or deleted.
+        </p>
+        <button className='btn btn-aurora btn-lg'>Go To Home</button>
+      </div>
+    </section>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
