c6a801adba0f31220579999933f00c37da5358d8
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-16 17:43:52 +0200

    create contact-us page

---
+++ src/pages/contact-us.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import Location from '../components/Location';
+
+function ContactUs() {
+  return (
+    <>
+      <Banner title='contact us' subtitle='find us' />
+      <Location />
+    </>
+  );
+}
+
+export default ContactUs;
