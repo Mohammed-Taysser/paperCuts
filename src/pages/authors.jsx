ff22b716ce5a67a97bf94c25a96ffd0affd6fabb
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-18 08:15:09 +0200

    add animation for authors

--- src/pages/authors.jsx
+++ src/pages/authors.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
+import SingleMember from '../components/SingleMember';
 import Banner from '../components/Banner';
 
 function Authors() {
   const authors_list = () => {
-    return (
-      <div className='col-sm-6 col-md-4 col-lg-3 my-3'>
-        <div className='text-center'>
-          <img
-            src='https://chapterone.qodeinteractive.com/wp-content/uploads/2019/08/author1.png'
-            alt='author name'
-            className='img-fluid rounded-circle'
-            width={200}
-            height={200}
-          />
-          <h4 className='my-2'>author name</h4>
-        </div>
-      </div>
-    );
+    return [1, 2, 3, 4].map((member) => {
+      return <SingleMember key={member.id} />;
+    });
   };
   return (
     <>
