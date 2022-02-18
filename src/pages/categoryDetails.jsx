ec0456b7f2bd06590c040c3dfb93f66746d8fd0b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-18 13:32:05 +0200

    update category-details with json-server

--- src/pages/categoryDetails.jsx
+++ src/pages/categoryDetails.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BooksAPI, BOOKS } from '../api/Localhost';
+import { useParams } from 'react-router-dom';
 import Banner from '../components/Banner';
 import SingleBook from '../components/SingleBook';
-import { LATEST_BOOKS } from '../api/Localhost';
+import JsonServerToast from '../components/JsonServerToast';
+import Alert from '../components/bootstrap-component/Alert';
 
-function categoryDetails() {
+function CategoryDetails() {
+  const { title: categoryTitle } = useParams();
+  const [jsonServerIsDown, setJsonServerIsDown] = useState(false);
+  const [booksByCategory, setBooksByCategory] = useState([]);
+
+  useEffect(() => {
+    api_get_books();
+  }, []);
+
+  const api_get_books = async () => {
+    await BooksAPI.get('/')
+      .then(function (response) {
+        // handle success
+        setJsonServerIsDown(false);
+        setBooksByCategory(get_category_books(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        if (error.toString() === 'Error: Network Error') {
+          setJsonServerIsDown(true);
+          setBooksByCategory(get_category_books(BOOKS));
+        }
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  const get_category_books = (response_book) => {
+    return response_book.filter((book) =>
+      book.category.includes(categoryTitle)
+    );
+  };
+
   const book_list = () => {
-    if (LATEST_BOOKS.length > 0) {
-      return LATEST_BOOKS.map((book) => {
+    if (booksByCategory.length > 0) {
+      return booksByCategory.map((book) => {
         return <SingleBook book={book} key={book.id} />;
       });
     }
-    return <> no books available </>;
+    return (
+      <Alert color='warning'>
+        no books available with '{categoryTitle}' category
+      </Alert>
+    );
   };
 
   return (
     <>
+      {jsonServerIsDown && <JsonServerToast />}
       <Banner title='category title' subtitle='shop list' />
       <section className='author-books my-5 py-5'>
         <div className='container p-xs-0 px-5 px-md-0'>
@@ -27,4 +68,4 @@ function categoryDetails() {
   );
 }
 
-export default categoryDetails;
+export default CategoryDetails;
