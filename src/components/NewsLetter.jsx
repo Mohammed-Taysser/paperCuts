bb9cd89c19c10670d10714444a1288ee64ce627f
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-18 08:47:59 +0200

    add sleepy-girl

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NewsLetterImage from '../assets/img/background/news-letter-bg.png';
+import SleepyGirlImage from '../assets/img/shapes/sleepy-girl.png';
 
 function NewsLetter() {
   return (
@@ -11,6 +12,7 @@ function NewsLetter() {
       <div className='wrapper text-center'>
         <div className='row justify-content-center'>
           <div className='col-md-8'>
+            <img src={SleepyGirlImage} alt="sleepy-girl" className='img-fluid d-block mx-auto mb-4' width={150} height={190} />
             <small className='special-small-title'>Get paperCuts</small>
             <h2 className='display-4'>Pen your bestseller</h2>
             <p className='my-4'>
