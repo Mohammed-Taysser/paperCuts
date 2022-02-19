03accca5e26ec1533cbd8e645324237262182afb
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-19 23:20:34 +0200

    update category-details page

--- src/pages/categoryDetails.jsx
+++ src/pages/categoryDetails.jsx
@@ -1,60 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { BooksAPI, BOOKS } from '../api/Localhost';
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  BOOKS_CATEGORY,
+  get_category_by_id,
+  CategoryAPI,
+} from '../api/Localhost';
 import { useParams } from 'react-router-dom';
 import Banner from '../components/Banner';
 import SingleBook from '../components/SingleBook';
 import Alert from '../components/bootstrap-component/Alert';
+import useJsonServerToast from '../context/IsJsonServerDown';
 
 function CategoryDetails() {
-  const { title: categoryTitle } = useParams();
-  const [jsonServerIsDown, setJsonServerIsDown] = useState(false);
+  const { id: categoryId } = useParams();
+  const is_jsonServer_down = useContext(useJsonServerToast);
+  const [currentCategory, setCurrentCategory] = useState(null);
   const [booksByCategory, setBooksByCategory] = useState([]);
 
   useEffect(() => {
-    api_get_books();
-  }, []);
+    if (is_jsonServer_down) {
+      let get_category = get_category_by_id(categoryId);
+      if (get_category) {
+        setCurrentCategory(get_category);
+        setBooksByCategory(BOOKS_CATEGORY);
+      }
+    } else {
+      api_get_category();
+    }
+  }, [is_jsonServer_down]);
 
-  const api_get_books = async () => {
-    await BooksAPI.get('/')
-      .then(function (response) {
+  const api_get_category = async () => {
+    await CategoryAPI.get(`/${categoryId}`)
+      .then((response) => {
         // handle success
-        setJsonServerIsDown(false);
-        setBooksByCategory(get_category_books(response.data));
+        setCurrentCategory(response.data);
+        setBooksByCategory(response.data.books);
       })
-      .catch(function (error) {
+      .catch((error) => {
         // handle error
-        if (error.toString() === 'Error: Network Error') {
-          setJsonServerIsDown(true);
-          setBooksByCategory(get_category_books(BOOKS));
-        }
       })
-      .then(function () {
+      .then(() => {
         // always executed
       });
   };
 
-  const get_category_books = (response_book) => {
-    return response_book.filter((book) =>
-      book.category.includes(categoryTitle)
-    );
-  };
-
   const book_list = () => {
-    if (booksByCategory.length > 0) {
-      return booksByCategory.map((book) => {
-        return <SingleBook book={book} key={book.id} />;
-      });
+    if (currentCategory) {
+      if (booksByCategory.length > 0) {
+        return booksByCategory.map((book, index) => {
+          return (
+            <SingleBook
+              book={{
+                id: book.id,
+                title: book.title,
+                img: book.img,
+                author: book.author.name,
+                price: book.price,
+                stars: book.stars,
+              }}
+              key={index}
+            />
+          );
+        });
+      }
+      return (
+        <Alert color='warning'>
+          no books available with '{currentCategory.title}' category
+        </Alert>
+      );
     }
-    return (
-      <Alert color='warning'>
-        no books available with '{categoryTitle}' category
-      </Alert>
-    );
+    return <Alert color='warning'>no category found</Alert>;
   };
 
   return (
     <>
-      <Banner title='category title' subtitle='shop list' />
+      <Banner
+        title={currentCategory ? currentCategory.title : 'category'}
+        subtitle='shop list'
+      />
       <section className='author-books my-5 py-5'>
         <div className='container p-xs-0 px-5 px-md-0'>
           <div className='row mt-4 justify-content-center align-items-center align-items-stretch'>
