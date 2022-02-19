4bd77c68864ec9204b33235b6d0713774ce23a3b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-19 18:27:21 +0200

    create hooks for auth system

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import MainRoute from './routes';
 import AuthContext from './context/auth';
-import JsonServerToast from './components/JsonServerToast';
+import IsJsonServerDownContext from './context/IsJsonServerDown';
+import useJsonServerToast from './hooks/useJsonServerToast';
+import useAuthContext from './hooks/useAuthContext';
 
 function App() {
+  const [isDown, jsonServerToast] = useJsonServerToast();
+  const auth_context_data = useAuthContext();
+
   useEffect(() => {
     window.onload = function () {
       document.body.classList.remove('load');
     };
   }, []);
 
-  let is_auth_default = false,
-    user_id_default = null;
-
-  if (localStorage.getItem('auth') !== null) {
-    const auth_localStorage = JSON.parse(localStorage.getItem('auth'));
-    is_auth_default = auth_localStorage.isAuth;
-    user_id_default = auth_localStorage.userId;
-  }
-
-  const [isAuth, setIsAuth] = useState(is_auth_default);
-  const [userId, setUserId] = useState(user_id_default);
-  const auth_context_data = { isAuth, userId, setIsAuth, setUserId };
-
   return (
     <AuthContext.Provider value={auth_context_data}>
       <BrowserRouter>
         <Navbar />
-        {<JsonServerToast />}
-        <MainRoute />
+        {isDown && jsonServerToast}
+        <IsJsonServerDownContext.Provider value={isDown}>
+          <MainRoute />
+        </IsJsonServerDownContext.Provider>
         <Footer />
       </BrowserRouter>
     </AuthContext.Provider>
