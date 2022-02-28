3f0b54b41c9028e79846537fd87b137a0d9e0ed6
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-28 17:37:02 +0200

    move logic from app.jsx to auth context

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -1,3 +1,32 @@
-import { createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
-export default createContext();
+const Context = createContext({
+  isAuth: null,
+  userId: null,
+  setIsAuth: (data) => console.log(data),
+  setUserId: (data) => console.log(data),
+});
+
+const AuthContext = (props) => {
+  let default_is_auth = false,
+    default_user_id = null;
+
+  if (localStorage.getItem('auth') !== null) {
+    const auth_localStorage = JSON.parse(localStorage.getItem('auth'));
+    default_is_auth = auth_localStorage.isAuth;
+    default_user_id = auth_localStorage.userId;
+  }
+
+  const [isAuth, setIsAuth] = useState(default_is_auth);
+  const [userId, setUserId] = useState(default_user_id);
+  const auth_context_data = { isAuth, userId, setIsAuth, setUserId };
+
+  return (
+    <Context.Provider value={auth_context_data}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+export default AuthContext;
+export { Context };
