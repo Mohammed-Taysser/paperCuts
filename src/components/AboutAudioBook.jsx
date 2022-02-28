d8fa1d9cc71daf80b372571c8590f71f45693ac5
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-28 17:40:01 +0200

    update feathers to be dynamic

--- src/components/AboutAudioBook.jsx
+++ src/components/AboutAudioBook.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { FiCheckCircle } from 'react-icons/fi';
-import AboutAudioBookImage from '../assets/img/background/about-audio-book.png'
+import AboutAudioBookImage from '../assets/img/background/about-audio-book.png';
 
 function AboutAudioBook() {
+  const audioBookFeathers = [
+    'Easy audio embed',
+    'Soundcloud playlists',
+    'User dashboard',
+    'Shopping cart',
+    'Set of shop layouts',
+    'Order page',
+    'External products',
+    'Virtual products',
+    'Variable products',
+    'Downloadable',
+  ];
+
   return (
     <section className='about-audio-book my-5 py-5'>
       <div className='container'>
@@ -14,55 +27,23 @@ function AboutAudioBook() {
               paperCuts is equipped with layouts for various product types, &
               you can feature audiobooks too!
             </p>
-            <div className='d-flex justify-content-between feather-container'>
-              <div className='text-muted'>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Easy audio embed</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Soundcloud playlists</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>User dashboard</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Shopping cart</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Set of shop layouts</span>
-                </div>
-              </div>
-              <div className='text-muted'>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Order page</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Virtual products</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>External products</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Variable products</span>
-                </div>
-                <div className='inner-feather my-2'>
-                  <FiCheckCircle className='text-aurora' />
-                  <span className='mx-2'>Downloadable</span>
-                </div>
-              </div>
+            <div className='row justify-content-center align-items-center text-muted '>
+              {audioBookFeathers.map((feather, index) => {
+                return (
+                  <div className='col-6 my-2' key={index}>
+                    <FiCheckCircle className='text-aurora' />
+                    <span className='mx-2'>{feather}</span>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div className="col-lg-7 my-3">
-<img src={AboutAudioBookImage} alt="about-audio-book" className='img-fluid' />
+          <div className='col-lg-7 my-3'>
+            <img
+              src={AboutAudioBookImage}
+              alt='about-audio-book'
+              className='img-fluid'
+            />
           </div>
         </div>
       </div>
