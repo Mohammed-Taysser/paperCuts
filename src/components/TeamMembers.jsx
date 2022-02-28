7160599e7e816287c45bfdf9174c93f32c413d1e
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-28 17:37:49 +0200

    use json-server data

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import SectionTitle from './SectionTitle';
 import SingleMember from './SingleMember';
+import { TEAM_MEMBER, TeamMemberAPI } from '../api/Localhost';
+import IsJsonServerDown from '../context/IsJsonServerDown';
 
 function TeamMembers() {
+  const is_jsonServer_down = useContext(IsJsonServerDown);
+  const [teamMember, setTeamMember] = useState(TEAM_MEMBER);
+
+  useEffect(() => {
+    if (is_jsonServer_down) {
+      setTeamMember(TEAM_MEMBER);
+    } else {
+      get_team_member_api();
+    }
+  }, []);
+
+  const get_team_member_api = () => {
+    TeamMemberAPI.get('/')
+      .then((response) => {
+        setTeamMember(response.data);
+      })
+      .catch((error) => {
+        setTeamMember(TEAM_MEMBER);
+      });
+  };
+
   const team_member_list = () => {
-    return [1, 2, 3, 4].map((member) => {
-      return <SingleMember key={member.id} />;
-    });
+    if (teamMember.length > 0) {
+      return teamMember.map((member) => {
+        return <SingleMember key={member.id} member={member} />;
+      });
+    } else {
+      return <> no team member available </>;
+    }
   };
   return (
     <>
-      <section className='py-5 my-5'>
-        <SectionTitle title='our amazing team' subtitle='info' />
+      <section className='my-5'>
+        <SectionTitle title='our amazing team' subtitle="what's up" />
         <div className='container my-5'>
           <div className='row justify-content-center align-items-center'>
             {team_member_list()}
