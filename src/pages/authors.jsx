29ae35f9fd3481efd8e1ad538b2b4cade72a0f15
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-28 18:58:03 +0200

    update static data with dynamic

--- src/pages/authors.jsx
+++ src/pages/authors.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import SingleMember from '../components/SingleMember';
 import Banner from '../components/Banner';
+import { AuthorsAPI, AUTHORS } from '../api/Localhost';
+import IsJsonServerDown from '../context/IsJsonServerDown';
 
 function Authors() {
+  const [authors, setAuthors] = useState(AUTHORS);
+  const is_jsonServer_down = useContext(IsJsonServerDown);
+
+  useEffect(() => {
+    if (is_jsonServer_down) {
+      setAuthors(AUTHORS);
+    } else {
+      get_author_api();
+    }
+  }, [is_jsonServer_down]);
+
+  const get_author_api = () => {
+    AuthorsAPI.get(`/`)
+      .then((response) => {
+        setAuthors(response.data);
+      })
+      .catch((err) => {
+        setAuthors(AUTHORS);
+      });
+  };
+
   const authors_list = () => {
-    return [1, 2, 3, 4].map((member) => {
-      return <SingleMember key={member.id} />;
+    return authors.map((member) => {
+      const config = {
+        name: `${member.first_name} ${member.last_name}`,
+        avatar: member.img,
+        position: member.email,
+      };
+      return <SingleMember key={member.id} member={config} url_id={member.id} />;
     });
   };
   return (
