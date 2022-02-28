5b8d3c19b63cbf1d0b46fa4fe7bb195ec0b7223b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-28 17:38:23 +0200

    add more setup

--- src/components/StarRate.jsx
+++ src/components/StarRate.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
 
-function StarRating() {
+function StarRating(props) {
   const [rating, setRating] = useState(1);
   const [hover, setHover] = useState(1);
+  useEffect(() => {
+    setRating(props.length);
+    setHover(props.length);
+  }, [props.length]);
   return (
     <div className='star-rating'>
       {[...Array(5)].map((star, index) => {
@@ -12,8 +16,13 @@ function StarRating() {
           <button
             type='button'
             key={index}
-            className={`btn btn-sm ${index <= (hover || rating) ? 'text-warning' : 'text-muted'}`}
-            onClick={() => setRating(index)}
+            className={`btn btn-sm ${
+              index <= (hover || rating) ? 'text-warning' : 'text-muted'
+            }`}
+            onClick={() => {
+              setRating(index);
+              props.onClick(index);
+            }}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
           >
@@ -26,5 +35,12 @@ function StarRating() {
     </div>
   );
 }
+
+StarRating.defaultProps = {
+  length: 1,
+  onClick: (data) => {
+    console.log(data);
+  },
+};
 
 export default StarRating;
