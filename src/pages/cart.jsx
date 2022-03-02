10a6c0db163a9ab5fa466540aac8fb13cd1c55ea
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-02 08:46:48 +0200

    finish cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,42 +1,31 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { FaMinus, FaPlus } from 'react-icons/fa';
 import Banner from '../components/Banner';
-import { BOOKS } from '../api/Localhost';
+import { CartAPI } from '../api/Localhost';
+import QuantityControlButton from '../components/QuantityControlButton';
+import useJsonServerToast from '../context/IsJsonServerDown';
+import 'bootstrap/js/src/alert';
+import CartCoupon from '../components/CartCoupon';
+import useCart from '../hooks/useCart';
 
 function Cart() {
-  const [quantityNumber, setQuantityNumber] = useState(1);
+  const [SHIPPING_PRICE, COUPON_PRICE, TOTAL_CART_PRICE, cartItems] = useCart();
+  const is_jsonServer_down = useContext(useJsonServerToast);
 
-  const onQuantityAdd = () => {
-    if (quantityNumber >= 1) {
-      setQuantityNumber(quantityNumber + 1);
-    }
+  const onQuantityChange = (quantity, book_id) => {
+    set_quantity_api(quantity, book_id);
   };
 
-  const onQuantityMinus = () => {
-    if (quantityNumber > 1) {
-      setQuantityNumber(quantityNumber - 1);
+  const set_quantity_api = (quantity, book_id) => {
+    if (!is_jsonServer_down) {
+      CartAPI.patch(`/${book_id}`, { quantity })
+        .then((response) => {
+          // console.log('changed');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  };
-
-  const quantity_controls = () => {
-    return (
-      <div className='d-flex align-items-center qty-container'>
-        <button className='btn-qty btn-qty-down' onClick={onQuantityAdd}>
-          <FaPlus />
-        </button>
-        <input
-          className='form-qty'
-          type='number'
-          name='quantity'
-          value={quantityNumber}
-          onChange={(e) => setQuantityNumber(parseInt(e.target.value, 10))}
-        />
-        <button className='btn-qty btn-qty-up' onClick={onQuantityMinus}>
-          <FaMinus />
-        </button>
-      </div>
-    );
   };
 
   const books_table = () => {
@@ -52,7 +41,7 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          {BOOKS.map((book, index) => {
+          {cartItems.map((book, index) => {
             return (
               <tr key={index}>
                 <th scope='row'>{index + 1}</th>
@@ -69,8 +58,18 @@ function Cart() {
                   </Link>
                 </td>
                 <td className='td-price'> {book.price}$ </td>
-                <td className='td-quantity'>{quantity_controls()}</td>
-                <td className='td-total'>{book.price * (index + 1)}$</td>
+                <td className='td-quantity'>
+                  <QuantityControlButton
+                    initQuantity={book.quantity}
+                    disabled={is_jsonServer_down}
+                    onQuantityChange={(quantity) =>
+                      onQuantityChange(quantity, book.id)
+                    }
+                  />
+                </td>
+                <td className='td-total'>
+                  {(book.price * book.quantity).toFixed(2)}$
+                </td>
               </tr>
             );
           })}
@@ -78,68 +77,74 @@ function Cart() {
       </table>
     );
   };
+
+  const basket_table = () => {
+    const total_to_pay = SHIPPING_PRICE + TOTAL_CART_PRICE - COUPON_PRICE;
+    let show_price = null;
+    if (total_to_pay > 0) {
+      show_price = total_to_pay.toFixed(2);
+    } else {
+      show_price = (
+        <>
+          <small className='text-decoration-line-through'>
+            {(SHIPPING_PRICE + TOTAL_CART_PRICE).toFixed(2)}
+          </small>{' '}
+          <strong>Free</strong>
+        </>
+      );
+    }
+    return (
+      <table className='table'>
+        <tbody>
+          <tr>
+            <td>
+              <h6>Subtotal</h6>
+            </td>
+            <td>
+              <span> ${TOTAL_CART_PRICE.toFixed(2)}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h6>Shipping</h6>
+            </td>
+            <td>
+              <span> Shipping Flat rate: ${SHIPPING_PRICE}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h6>Total</h6>
+            </td>
+            <td>
+              <span className='text-aurora'>${show_price}</span>
+              {COUPON_PRICE > 0 && (
+                <small className='text-warning ms-2'>(-{COUPON_PRICE})</small>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  };
+
   return (
     <>
       <Banner title='cart' subtitle='info' />
       <section className='cart-page my-5 py-5'>
         <div className='container'>
           <div className='table-responsive'>{books_table()}</div>
-          <div className='my-5'>
-            <form className='row g-3'>
-              <div className='col-auto'>
-                <label htmlFor='coupon-input-id' className='visually-hidden'>
-                  coupon
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='coupon-input-id'
-                  placeholder='Coupon Code'
-                  required
-                />
+          <CartCoupon />
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='cart-total'>
+                <h2 className='my-3'>Basket totals</h2>
+                <div className='table-responsive'>{basket_table()}</div>
+                <Link to='/checkout' className='btn btn-aurora btn-lg mt-4'>
+                  process to checkout
+                </Link>
               </div>
-              <div className='col-auto'>
-                <button type='submit' className='btn btn-aurora mb-3'>
-                  Apply Coupon
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className='cart-total'>
-            <h2 className='my-3'>Basket totals</h2>
-            <div className='table-responsive'>
-              <table className='table'>
-                <tbody>
-                  <tr>
-                    <td>
-                      <h6>Subtotal</h6>
-                    </td>
-                    <td>
-                      <span> $174.00</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <h6>Shipping</h6>
-                    </td>
-                    <td>
-                      <span> Shipping Flat rate: $79.00</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <h6>Total</h6>
-                    </td>
-                    <td>
-                      <span className='text-aurora'> $253.00</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
-            <Link to='/checkout' className='btn btn-aurora btn-lg mt-4'>
-              process to checkout
-            </Link>
           </div>
         </div>
       </section>
