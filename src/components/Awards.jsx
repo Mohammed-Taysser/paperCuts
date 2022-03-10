5c9b752c16bf8026ec46f5e30346361a053e3b59
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-10 10:38:45 +0200

    update image

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import Client1 from '../assets/img/client-1.png';
 import Client2 from '../assets/img/client-2.png';
 import Client3 from '../assets/img/client-3.png';
-import Writer from '../assets/img/writer.png';
 import SkewedShape from '../assets/img/skewed-shape.png';
-import InterviewWord from '../assets/img/interview-word.png';
-import ManShape from '../assets/img/man-shape.png';
-import TopRoundedShape from '../assets/img/top-rounded-shape.png';
-import PopShape from '../assets/img/pop-shape.png';
-import BookShelf from '../assets/img/book-shelf.jpg';
+import AwardsImage from '../assets/img/background/awards.png';
 
 function Awards() {
   return (
@@ -17,50 +12,7 @@ function Awards() {
         <div className='row justify-content-between'>
           <div className='col-md-6 my-3'>
             <div className='writer-container'>
-              <div className='writer-img'>
-                <img
-                  src={TopRoundedShape}
-                  alt='top-rounded-shape'
-                  width={'270'}
-                  height={'280'}
-                  className='top-rounded-shape'
-                />
-                <img
-                  src={PopShape}
-                  alt='pop-shape'
-                  width={'240'}
-                  height={'250'}
-                  className='pop-shape'
-                />
-                <img
-                  src={ManShape}
-                  alt='man-shape'
-                  width={'110'}
-                  height={'185'}
-                  className='man-shape'
-                />
-                <img
-                  src={BookShelf}
-                  alt='book-shelf'
-                  width={'180'}
-                  height={'180'}
-                  className='book-shelf'
-                />
-                <img
-                  src={Writer}
-                  alt='writer'
-                  width={'300'}
-                  height={'300'}
-                  className='img-fluid'
-                />
-              </div>
-              <img
-                src={InterviewWord}
-                alt='interview-word'
-                className='img-fluid d-block ms-auto'
-                width={'180'}
-                height={'90'}
-              />
+              <img src={AwardsImage} alt='awards' />
             </div>
           </div>
           <div className='col-md-6 my-3'>
@@ -75,7 +27,7 @@ function Awards() {
             <h3 className='mb-3'>Awards & nominations</h3>
             <p className='text-muted'>
               This open access wide-ranging collation of papers examines a host
-              of issues in studying immigrant.{' '}
+              of issues in studying immigrant.
             </p>
             <div className='clients mt-4'>
               <img
