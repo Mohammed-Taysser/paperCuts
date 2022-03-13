9baf84f0e8290e1003d001635267eb68b79faa52
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-13 09:43:08 +0200

    update json-server toast

--- src/hooks/useJsonServerToast.jsx
+++ src/hooks/useJsonServerToast.jsx
@@ -16,9 +16,8 @@ function useJsonServerToast(initValue = true) {
 
   const toast = (
     <Toast>
-      json-server is down, please make sure it's work.
-      <span className='bg-aurora text-light'> any data are static </span>. You
-      can't create, edit or delete unless it work. learn how to work with
+      json-server is down, please make sure it's work. You can't create, edit or
+      delete unless it work. learn how to work with
       <a
         href='https://github.com/Mohammed-Taysser/paperCuts#json-server'
         className='mx-2'
