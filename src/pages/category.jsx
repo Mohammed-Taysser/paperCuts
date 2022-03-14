9db2f12ece4862cd4fa25cdce6b71863be174a61
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-14 15:02:50 +0200

    refactor pages

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,55 +1,61 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Banner from '../components/Banner';
-import useJsonServerToast from '../context/IsJsonServerDown';
+import Spinner from '../components/bootstrap-component/Spinner';
 import { CATEGORY, CategoryAPI } from '../api/Localhost';
 
 function Category() {
-  const is_jsonServer_down = useContext(useJsonServerToast);
-  const [category, setCategory] = useState(CATEGORY);
+  const [category, setCategory] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (is_jsonServer_down) {
-      setCategory(CATEGORY);
-    } else {
-      api_get_category();
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    api_get_category();
   }, []);
 
   const api_get_category = async () => {
     await CategoryAPI.get('/')
-      .then(function (response) {
-        // handle success
+      .then((response) => {
         setCategory(response.data);
       })
-      .catch(function (error) {
-        // handle error
+      .catch((error) => {
+        setCategory(CATEGORY);
       })
-      .then(function () {
-        // always executed
+      .then(() => {
+        setLoading(false);
       });
   };
 
-  const category_list = () => {
-    return category.map((cty) => {
-      return (
-        <div className='col-6 col-md-4 col-lg-3 my-3' key={cty.id}>
-          <div className='card border-0 nice-shadow h-100 single-category p-4 pb-2'>
-            <div className='img'>
-              <img src={cty.img} className='card-img-top' alt={cty.title} />
-            </div>
-            <div className='card-body text-center'>
-              <h5 className='card-title m-0'>
-                <Link to={`/category/${cty.id}`} className='stretched-link'>
-                  {cty.title}
-                </Link>
-              </h5>
+  const CategoryList = () => {
+    if (category.length > 0) {
+      let category_items = category.map((cty) => {
+        return (
+          <div className='col-6 col-md-4 col-lg-3 my-3' key={cty.id}>
+            <div className='card border-0 nice-shadow h-100 single-category p-4 pb-2'>
+              <div className='img'>
+                <img src={cty.img} className='card-img-top' alt={cty.title} />
+              </div>
+              <div className='card-body text-center'>
+                <h5 className='card-title m-0'>
+                  <Link to={`/category/${cty.slug}`} className='stretched-link'>
+                    {cty.title}
+                  </Link>
+                </h5>
+              </div>
             </div>
           </div>
+        );
+      });
+      return (
+        <div className='row justify-content-center align-items-center'>
+          {category_items}
         </div>
       );
-    });
+    }
+    return <> no category </>;
+  };
+
+  const RenderCategories = () => {
+    return loading ? <Spinner /> : <CategoryList />;
   };
 
   return (
@@ -57,9 +63,7 @@ function Category() {
       <Banner title='our category' subtitle='our space' />
       <section className='my-5 py-5'>
         <div className='container'>
-          <div className='row justify-content-center align-items-center'>
-            {category_list()}
-          </div>
+          <RenderCategories />
         </div>
       </section>
     </>
