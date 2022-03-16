4720aeb360afbdbacc663aaf96ede1155a2ed855
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-16 21:07:10 +0200

    create component to get books by category

---
+++ src/components/GetBookByCategory.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import SingleBook from './single/SingleBook';
+import Alert from './bootstrap/Alert';
+import Spinner from './bootstrap/Spinner';
+import { BooksAPI, BOOKS } from '../api/Localhost';
+
+/**
+ * Get book depend on the props:
+ * 1. pass nothing => get random 4 books
+ * 2. getBy= categoryId    => get category books by id
+ * 3. getBy= categoryArray => get books by array of id's
+ *
+ * @param {Object} props needed props
+ * @returns {JSX.Element} books
+ */
+function GetBookByCategory(props) {
+  const { getBy = null } = props;
+  const [books, setBooks] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api_get_books();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const api_get_books = async () => {
+    await BooksAPI.get('/')
+      .then((response) => {
+        filter_books(response.data);
+      })
+      .catch((error) => {
+        filter_books(BOOKS);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const filter_books = (response_books) => {
+    let filterd_books = null;
+
+    switch (typeof getBy) {
+      case 'number':
+        filterd_books = response_books.filter((book) =>
+          book.category.includes(props.getBy)
+        );
+        break;
+      case 'object':
+        filterd_books = response_books.filter((book) =>
+          props.getBy.includes(book.id)
+        );
+        break;
+      default:
+        filterd_books = shuffle_arr(response_books).slice(0, 4);
+        break;
+    }
+
+    setBooks([...new Set(filterd_books)]);
+  };
+
+  function shuffle_arr(arr) {
+    return arr.sort(() => 0.5 - Math.random());
+  }
+
+  const Render = () => {
+    if (loading) {
+      return <Spinner />;
+    }
+
+    if (books && books.length > 0) {
+      let books_list = books.map((book, index) => (
+        <SingleBook book={book} key={index} />
+      ));
+      return (
+        <div className='row justify-content-center align-items-stretch'>
+          {books_list}
+        </div>
+      );
+    } else {
+      return <Alert>no available books found</Alert>;
+    }
+  };
+
+  return <Render />;
+}
+
+export default GetBookByCategory;
