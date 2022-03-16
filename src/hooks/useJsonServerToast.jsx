ecff9064fd4e341e539b83034c2d6b4d75d5e7cd
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-16 14:27:10 +0200

    drop useAxios hook

--- src/hooks/useJsonServerToast.jsx
+++ src/hooks/useJsonServerToast.jsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import Toast from '../components/bootstrap-component/Toast';
-import useAxios from './useAxios';
+import base_url from '../api/Localhost';
+import Toast from '../components/bootstrap/Toast';
 
 function useJsonServerToast(initValue = true) {
   const [isDown, setIsDown] = useState(initValue);
-  const { response } = useAxios();
 
   useEffect(() => {
-    if (response !== null) {
-      setIsDown(false);
-    } else {
-      setIsDown(true);
-    }
-  }, [response]);
+    api_send_request();
+  }, []);
+
+  const api_send_request = async () => {
+    await base_url
+      .get('/')
+      .then((response) => {
+        setIsDown(false);
+      })
+      .catch((error) => {
+        setIsDown(true);
+      });
+  };
 
   const toast = (
     <Toast>
@@ -20,7 +26,7 @@ function useJsonServerToast(initValue = true) {
       delete unless it work. learn how to work with
       <a
         href='https://github.com/Mohammed-Taysser/paperCuts#json-server'
-        className='mx-2'
+        className='mx-1'
         target='_blank'
         rel='noreferrer'
       >
