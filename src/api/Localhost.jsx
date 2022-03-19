9b409e2194b70d42106b65b8f469e0275db4c565
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-03-19 13:47:44 +0200

    remove un-needed api

--- src/api/Localhost.jsx
+++ src/api/Localhost.jsx
@@ -30,17 +30,17 @@ const CartAPI = axios.create({
   baseURL: 'http://localhost:8080/cart',
 });
 
-// const CouponAPI = axios.create({
-//   baseURL: 'http://localhost:8080/coupons',
-// });
+const CouponAPI = axios.create({
+  baseURL: 'http://localhost:8080/coupons',
+});
 
-// const OrderAPI = axios.create({
-//   baseURL: 'http://localhost:8080/orders',
-// });
+const OrderAPI = axios.create({
+  baseURL: 'http://localhost:8080/orders',
+});
 
-// const EventAPI = axios.create({
-//   baseURL: 'http://localhost:8080/events',
-// });
+const EventAPI = axios.create({
+  baseURL: 'http://localhost:8080/events',
+});
 
 const ReviewAPI = axios.create({
   baseURL: 'http://localhost:8080/reviews',
@@ -48,6 +48,10 @@ const ReviewAPI = axios.create({
 
 const WishlistAPI = axios.create({
   baseURL: 'http://localhost:8080/wishlist',
+});
+
+const TestimonialsAPI = axios.create({
+  baseURL: 'http://localhost:8080/testimonials',
 });
 
 // exported variable names
@@ -58,15 +62,12 @@ const SERVICES = Data.services,
   TOP_FIVE = shuffle_arr(Data.books.slice(0, 5)),
   AUTHORS = Data.authors,
   WISHLIST = Data.wishlist,
-  //   ORDERS = Data.orders,
+  ORDERS = Data.orders,
   CART = Data.cart,
-  //   COUPONS = Data.coupons,
-  //   EVENTS = Data.events,
+  COUPONS = Data.coupons,
+  EVENTS = Data.events,
   TEAM = Data.team,
-  //   LATEST_BOOKS = shuffle_arr(Data.books.slice(0, 8)),
-  // RELATED_BOOKS = shuffle_arr(Data.books.slice(0, 4)),
-  //   BOOKS_CATEGORY = shuffle_arr(Data.books.slice(0, 4)),
-  BOOKS_REVIEWS = Data.books[0].reviews;
+  TESTIMONIALS = Data.testimonials;
 
 // // needed function
 
@@ -74,49 +75,37 @@ function shuffle_arr(arr) {
   return arr.sort(() => 0.5 - Math.random());
 }
 
-// function get_book_by_id(id = '1') {
-//   return BOOKS.find((book) => book.id.toString() === id);
-// }
-
 function get_book_by_slug(slug = '') {
   return BOOKS.find((book) => book.slug === slug);
 }
 
-// function get_category_by_id(id = '1') {
-//   return CATEGORY.find((cty) => cty.id.toString() === id);
-// }
-
-// function get_category_by_slug(slug = '') {
-//   return CATEGORY.find((cty) => cty.slug === slug);
-// }
+function get_category_by_slug(slug = '') {
+  return CATEGORY.find((cty) => cty.slug === slug);
+}
 
 function get_author_by_id(id = 1) {
   return AUTHORS.find((author) => author.id === id);
 }
 
-// function get_author_by_username(username = '') {
-//   return AUTHORS.find((author) => author.username === username);
-// }
+function get_author_by_username(username = '') {
+  return AUTHORS.find((author) => author.username === username);
+}
 
-// function get_author_by_email(email = '') {
-//   return AUTHORS.find((author) => author.email === email);
-// }
+function get_author_by_email(email = '') {
+  return AUTHORS.find((author) => author.email === email);
+}
 
-// function get_coupon_by_id(id = 1) {
-//   return COUPONS.find((coupon) => coupon.id === id);
-// }
+function get_coupon_by_title(title = '') {
+  return COUPONS.find((coupon) => coupon.title === title);
+}
 
-// function get_coupon_by_title(title = '') {
-//   return COUPONS.find((coupon) => coupon.title === title);
-// }
+function get_event_by_slug(slug = '') {
+  return EVENTS.find((event) => event.slug === slug);
+}
 
-// function get_event_by_id(id = '1') {
-//   return EVENTS.find((event) => event.id.toString() === id);
-// }
-
-// function get_order_by_id(id = '1') {
-//   return ORDERS.find((order) => order.id.toString() === id);
-// }
+function get_order_by_id(id = '1') {
+  return ORDERS.find((order) => order.id.toString() === id);
+}
 
 function get_reviews_by_id(id = 1) {
   return REVIEWS.filter((review) => review.id === id);
@@ -139,41 +128,35 @@ export {
   AuthorsAPI,
   CartAPI,
   WishlistAPI,
-  //   CouponAPI,
-  //   OrderAPI,
-  //   EventAPI,
+  CouponAPI,
+  OrderAPI,
+  EventAPI,
   ReviewAPI,
+  TestimonialsAPI,
 };
 export {
   SERVICES,
   REVIEWS,
   BOOKS,
-  //   CART,
+  CART,
   TOP_FIVE,
-  //   COUPONS,
-  //   AUTHORS,
+  AUTHORS,
   TEAM,
-  // LATEST_BOOKS,
-  // RELATED_BOOKS,
-  //   BOOKS_REVIEWS,
   CATEGORY,
-  //   ORDERS,
-  //   EVENTS,
-  //   BOOKS_CATEGORY,
+  ORDERS,
+  TESTIMONIALS,
+  EVENTS,
 };
 export {
   get_book_by_slug,
-  //   get_book_by_id,
   get_author_by_id,
   get_cart_by_id,
   get_reviews_by_id,
   get_wishlist_by_bookId,
-  //   get_category_by_id,
-  //   get_category_by_slug,
-  //   get_coupon_by_id,
-  //   get_coupon_by_title,
-  //   get_event_by_id,
-  // get_author_by_username,
-  //   get_author_by_email,
-  //   get_order_by_id,
+  get_category_by_slug,
+  get_coupon_by_title,
+  get_event_by_slug,
+  get_author_by_username,
+  get_author_by_email,
+  get_order_by_id,
 };
