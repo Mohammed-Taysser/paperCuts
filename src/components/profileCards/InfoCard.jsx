07246175a1fe07fab56526f674f9bc9aece03e0c
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-04-05 22:52:07 +0200

    add empty string on empty info

--- src/components/profileCards/InfoCard.jsx
+++ src/components/profileCards/InfoCard.jsx
@@ -3,13 +3,13 @@ import { Textarea } from '../bootstrap/Form';
 
 function InfoCard(props) {
   const { formData, onInputChange } = props;
-  
+
   return (
     <>
       <Textarea
         outer='col-md-6'
         rows={3}
-        value={formData['info']}
+        value={formData['info'] ? formData['info'] : ''}
         name='info'
         placeholder='info'
         onChange={onInputChange}
@@ -18,7 +18,7 @@ function InfoCard(props) {
         outer='col-md-6'
         rows={3}
         placeholder='extra info'
-        value={formData['extraInfo']}
+        value={formData['extraInfo'] ? formData['extraInfo'] : ''}
         name='extraInfo'
         onChange={onInputChange}
       />
