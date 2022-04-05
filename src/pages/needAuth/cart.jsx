c21f5e19ee10909f942b2702aabc76d32f126004
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-04-05 22:47:54 +0200

    refactor & update quantity change

--- src/pages/needAuth/cart.jsx
+++ src/pages/needAuth/cart.jsx
@@ -1,36 +1,96 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsCheck2Circle } from 'react-icons/bs';
-import { CartAPI } from '../../api/Localhost';
+import { CartAPI, get_cart_by_userId } from '../../api/Localhost';
+import { Context as CouponContext } from '../../context/coupon';
 import { Context as AuthContext } from '../../context/auth';
 import Banner from '../../components/standalone/Banner';
 import Quantity from '../../components/Quantity';
 import CartCoupon from '../../components/CartCoupon';
-import useCart from '../../hooks/useCart';
 import Alert from '../../components/bootstrap/Alert';
 import GetBookByCategory from '../../components/GetBookByCategory';
 import SectionTitle from '../../components/standalone/SectionTitle';
 import usePageTitle from '../../hooks/usePageTitle';
 
+const SHIPPING_PRICE = 50;
+
 function Cart() {
   usePageTitle('Cart');
+  const coupon_context = useContext(CouponContext);
   const auth_context = useContext(AuthContext);
-  const [SHIPPING_PRICE, COUPON_PRICE, TOTAL_CART_PRICE, CART_ITEMS] =
-    useCart();
+  const appliedCoupons = coupon_context.coupons || [];
+  const [userCart, setUserCart] = useState(null);
+  const [discount, setDiscount] = useState(0);
+  const [totalPay, setTotalPay] = useState(0);
   const [isLoading, setIsLoading] = useState({});
   const [isSaved, setIsSaved] = useState({});
 
+  useEffect(() => {
+    api_get_cart();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    let coupon_price = appliedCoupons.reduce((total, coupon) => {
+      total += coupon.value;
+      return total;
+    }, 0);
+
+    setDiscount(coupon_price);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [coupon_context.coupons]);
+
+  const api_get_cart = async () => {
+    await CartAPI.get(`/?userId=${auth_context.userData.id}`)
+      .then((response) => {
+        onCartItemsLoad(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+        onCartItemsLoad(get_cart_by_userId(auth_context.userData.id));
+      });
+  };
+
+  const onCartItemsLoad = (cart) => {
+    if (cart && cart.items) {
+      setUserCart(cart);
+      calculateDiscountAndTotalPay(cart.items);
+    }
+  };
+
+  const calculateDiscountAndTotalPay = (cart_items) => {
+    let total_cart_price = 0;
+    for (const book in cart_items) {
+      if (Object.hasOwnProperty.call(cart_items, book)) {
+        total_cart_price +=
+          cart_items[book]['quantity'] * cart_items[book]['price'];
+      }
+    }
+
+    let coupon_price = appliedCoupons.reduce((total, coupon) => {
+      total += coupon.value;
+      return total;
+    }, 0);
+
+    setDiscount(coupon_price);
+    setTotalPay(total_cart_price);
+  };
+
   const onQuantityChange = (quantity, bookId) => {
     setIsSaved({ ...isSaved, [bookId]: false });
-    let current_book = { ...CART_ITEMS[bookId], quantity };
-    set_quantity_api({ ...CART_ITEMS, [bookId]: current_book }, bookId);
+    let current_book = { ...userCart.items[bookId], quantity };
+    set_quantity_api({ ...userCart.items, [bookId]: current_book }, bookId);
   };
 
   const set_quantity_api = (items, bookId) => {
     setIsLoading({ ...isLoading, [bookId]: true });
-    CartAPI.patch(`/${auth_context.userData.id}`, { items })
+    CartAPI.patch(`/${userCart.id}`, { items })
       .then((response) => {
         setIsSaved({ ...isSaved, [bookId]: true });
+        let newCartItems = { ...userCart.items };
+        newCartItems[bookId].quantity = response.data.items[bookId].quantity;
+        setUserCart({ ...userCart, items: newCartItems });
+        calculateDiscountAndTotalPay(newCartItems);
       })
       .catch((error) => {
         console.log(error);
@@ -42,68 +102,70 @@ function Cart() {
 
   const delete_cart_api = (bookId) => {
     setIsLoading({ ...isLoading, [bookId]: true });
-    let items_without_book = { ...CART_ITEMS };
+    let items_without_book = { ...userCart.items };
     delete items_without_book[bookId];
     set_quantity_api(items_without_book, bookId);
   };
 
-  const RenderTableBooksRows = () => {
-    let cart_items_table = [],
-      index = 0;
-    for (const key in CART_ITEMS) {
-      if (Object.hasOwnProperty.call(CART_ITEMS, key)) {
-        index++;
-        cart_items_table.push(
-          <tr key={index}>
-            <th scope='row'>{index}</th>
-            <td className='td-title'>
-              <img
-                src={CART_ITEMS[key].image}
-                alt={CART_ITEMS[key].title}
-                className='img-fluid'
-                width={70}
-                height={100}
-              />
-              <Link to={`/books/${CART_ITEMS[key].slug}`} className='mx-2'>
-                {CART_ITEMS[key].title}
-              </Link>
-            </td>
-            <td className='td-price'> {CART_ITEMS[key].price}$ </td>
-            <td className='td-quantity'>
-              {isSaved[key] && (
-                <small className='text-muted'>
-                  <BsCheck2Circle className='h6 mb-1' /> updated
-                </small>
-              )}
-              <Quantity
-                loading={isLoading[key]}
-                initQuantity={CART_ITEMS[key].quantity}
-                onQuantityChange={(quantity) => onQuantityChange(quantity, key)}
-              />
-            </td>
-            <td className='td-total'>
-              {(CART_ITEMS[key].price * CART_ITEMS[key].quantity).toFixed(2)}$
-            </td>
-            <td className="td-price'">
-              <button
-                className='btn btn-sm btn-outline-danger text-center p-0 rounded-circle'
-                onClick={() => delete_cart_api(key)}
-                style={{ width: '25px', height: '25px' }}
-              >
-                X
-              </button>
-            </td>
-          </tr>
-        );
+  const RenderBooksTableRows = () => {
+    if (Object.keys(userCart.items).length > 0) {
+      let cart_items_table = [],
+        index = 0;
+      for (const key in userCart.items) {
+        if (Object.hasOwnProperty.call(userCart.items, key)) {
+          index++;
+          cart_items_table.push(
+            <tr key={index}>
+              <th scope='row'>{index}</th>
+              <td className='td-title'>
+                <img
+                  src={userCart.items[key].image}
+                  alt={userCart.items[key].title}
+                  className='img-fluid'
+                  width={70}
+                  height={100}
+                />
+                <Link
+                  to={`/books/${userCart.items[key].slug}`}
+                  className='mx-2'
+                >
+                  {userCart.items[key].title}
+                </Link>
+              </td>
+              <td className='td-price'> {userCart.items[key].price}$ </td>
+              <td className='td-quantity'>
+                {isSaved[key] && (
+                  <small className='text-muted'>
+                    <BsCheck2Circle className='h6 mb-1' /> updated
+                  </small>
+                )}
+                <Quantity
+                  loading={isLoading[key]}
+                  initQuantity={userCart.items[key].quantity}
+                  onQuantityChange={(quantity) =>
+                    onQuantityChange(quantity, key)
+                  }
+                />
+              </td>
+              <td className='td-total'>
+                {(
+                  userCart.items[key].price * userCart.items[key].quantity
+                ).toFixed(2)}
+                $
+              </td>
+              <td className="td-price'">
+                <button
+                  className='btn-close'
+                  onClick={() => delete_cart_api(key)}
+                  type='button'
+                ></button>
+              </td>
+            </tr>
+          );
+        }
       }
-    }
 
-    return <>{cart_items_table}</>;
-  };
-
-  const RenderBooksTable = () => {
-    if (Object.keys(CART_ITEMS).length > 0) {
-      return <RenderTableBooksRows />;
+      return <>{cart_items_table}</>;
     } else {
       return (
         <tr>
@@ -130,14 +192,14 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <RenderBooksTable />
+          <RenderBooksTableRows />
         </tbody>
       </table>
     );
   };
 
   const BasketTable = () => {
-    const total_to_pay = SHIPPING_PRICE + TOTAL_CART_PRICE - COUPON_PRICE;
+    const total_to_pay = SHIPPING_PRICE + totalPay - discount;
     let show_price = null;
     if (total_to_pay > 0) {
       show_price = total_to_pay.toFixed(2);
@@ -145,7 +207,7 @@ function Cart() {
       show_price = (
         <>
           <small className='text-decoration-line-through'>
-            {(SHIPPING_PRICE + TOTAL_CART_PRICE).toFixed(2)}
+            {(SHIPPING_PRICE + totalPay).toFixed(2)}
           </small>
           <strong>Free</strong>
         </>
@@ -159,7 +221,7 @@ function Cart() {
               <h6>Subtotal</h6>
             </td>
             <td>
-              <span> ${TOTAL_CART_PRICE.toFixed(2)}</span>
+              <span> ${totalPay.toFixed(2)}</span>
             </td>
           </tr>
           <tr>
@@ -176,8 +238,8 @@ function Cart() {
             </td>
             <td>
               <span className='text-aurora'>${show_price}</span>
-              {COUPON_PRICE > 0 && (
-                <small className='text-warning ms-2'>(-{COUPON_PRICE})</small>
+              {discount > 0 && (
+                <small className='text-warning ms-2'>(-{discount})</small>
               )}
             </td>
           </tr>
@@ -187,7 +249,7 @@ function Cart() {
   };
 
   const Render = () => {
-    if (CART_ITEMS && Object.keys(CART_ITEMS).length > 0) {
+    if (userCart && userCart.items && Object.keys(userCart.items).length > 0) {
       return (
         <>
           <div className='table-responsive'>
