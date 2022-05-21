7eef8545ace155593db664c4987059ed82b9b3fb
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-21 17:22:34 +0200

    refactor & update render function

--- src/pages/noAuth/authors.jsx
+++ src/pages/noAuth/authors.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import SingleAuthor from '../../components/single/SingleAuthor';
-import Banner from '../../components/standalone/Banner';
 import { RowOfPlaceholderCard } from '../../components/bootstrap/Placeholder';
-import { AuthorsAPI, AUTHORS } from '../../api/Localhost';
+import { getAllAuthors } from '../../api/authors';
+import SingleAuthor from '../../components/single/SingleAuthor';
 import usePageTitle from '../../hooks/usePageTitle';
+import Alert from '../../components/bootstrap/Alert';
+import WithBanner from '../../layout/WithBanner';
 
 function Authors() {
   usePageTitle('Authors');
   const [authors, setAuthors] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
 
   useEffect(() => {
     get_author_api();
   }, []);
 
-  const get_author_api = () => {
-    AuthorsAPI.get(`/`)
+  const get_author_api = async () => {
+    await getAllAuthors()
       .then((response) => {
         setAuthors(response.data);
       })
-      .catch((err) => {
-        setAuthors(AUTHORS);
+      .catch(() => {
+        setLoadingError(true);
       })
       .finally(() => {
         setLoading(false);
@@ -28,36 +30,36 @@ function Authors() {
   };
 
   const AuthorsList = () => {
-    if (authors.length > 0) {
-      let authors_list = authors.map((author) => (
-        <SingleAuthor key={author.id} author={author} />
-      ));
-      return (
-        <div className='row justify-content-center align-items-center'>
-          {authors_list}
-        </div>
-      );
-    }
-    return <> no author found </>;
+    let authors_list = authors.map((author) => (
+      <SingleAuthor key={author._id} author={author} />
+    ));
+    return (
+      <div className='row justify-content-center align-items-center'>
+        {authors_list}
+      </div>
+    );
   };
 
-  const RenderMessage = () => {
+  const RenderAuthors = () => {
     if (loading) {
       return <RowOfPlaceholderCard num={8} />;
-    } else {
+    } else if (loadingError) {
+      return <Alert> Error While Loading Author </Alert>;
+    } else if (authors && authors.length > 0) {
       return <AuthorsList />;
+    } else {
+      return <Alert> no author found </Alert>;
     }
   };
 
   return (
-    <>
-      <Banner title='authors' subtitle='info' />
+    <WithBanner title='authors' subtitle='info'>
       <section className='my-5 py-5'>
         <div className='container'>
-          <RenderMessage />
+          <RenderAuthors />
         </div>
       </section>
-    </>
+    </WithBanner>
   );
 }
 
