f7f4352f208e5b9de79ed7020abcc5689f48393a
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-22 11:38:58 +0200

    add spinner.scss

--- src/components/bootstrap/Spinner.jsx
+++ src/components/bootstrap/Spinner.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../../assets/scss/components/spinner.scss';
 
 const Spinner = (props) => {
+  const spinnerClassName = `spinner-border text-${props.color} ${
+    props.sm ? 'spinner-border-sm' : ''
+  }`;
   return (
     <div className={`text-center ${props.className}`}>
-      <div className={`spinner-border text-${props.color}`} role='status'>
+      <div className={spinnerClassName} role='status'>
         <span className='visually-hidden'>{props.text}</span>
       </div>
       {props.text && <p className=''>{props.text}</p>}
