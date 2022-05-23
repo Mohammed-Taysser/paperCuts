eb30724bc03aeabb4bf08e352f575bb1a6ddd028
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-23 20:45:47 +0200

    update api call & error handel

--- src/pages/needAuth/wishlist.jsx
+++ src/pages/needAuth/wishlist.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { WishlistAPI, get_wishlist_by_userId } from '../../api/Localhost';
-import { Context as AuthContext } from '../../context/auth';
+import { getAllAuthorWishlist } from '../../api/wishlist.api';
 import { RowOfPlaceholderCard } from '../../components/bootstrap/Placeholder';
-import SingleBook from '../../components/single/SingleBook';
 import Banner from '../../components/standalone/Banner';
 import Alert from '../../components/bootstrap/Alert';
 import usePageTitle from '../../hooks/usePageTitle';
+import BookList from '../../components/standalone/BookList';
 
 function Wishlist() {
   usePageTitle('Wishlist');
-  const auth_context = useContext(AuthContext);
-  const [wishlistItems, setWishlistItems] = useState({});
+  const [wishlistItems, setWishlistItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(null);
 
   useEffect(() => {
     api_get_wishlist();
@@ -20,49 +19,25 @@ function Wishlist() {
   }, []);
 
   const api_get_wishlist = () => {
-    WishlistAPI.get(`?userId=${auth_context.userData.id}`)
+    getAllAuthorWishlist()
       .then((response) => {
-        if (response.data.length === 1) {
-          setWishlistItems(response.data[0].items);
-        } else {
-          setWishlistItems({});
-        }
+        setWishlistItems(response.data);
       })
       .catch((error) => {
-        console.log(error);
-        let temp_items = get_wishlist_by_userId(auth_context.userData.id);
-        if (temp_items) {
-          setWishlistItems(temp_items.items);
-        } else {
-          setWishlistItems({});
-        }
+        setLoadingError(error);
       })
       .finally(() => {
         setLoading(false);
       });
   };
 
-  const Render = () => {
+  const RenderWishlist = () => {
     if (loading) {
       return <RowOfPlaceholderCard num={6} />;
-    }
-    if (wishlistItems && Object.keys(wishlistItems).length > 0) {
-      let wish_list_items = [];
-
-      for (const bookId in wishlistItems) {
-        if (Object.hasOwnProperty.call(wishlistItems, bookId)) {
-          wish_list_items.push(
-            <SingleBook
-              book={{ ...wishlistItems[bookId], id: bookId }}
-              key={bookId}
-            />
-          );
-        }
-      }
-
-      return (
-        <div className='row justify-content-center'> {wish_list_items} </div>
-      );
+    } else if (loadingError) {
+      return <Alert>{loadingError}</Alert>;
+    } else if (wishlistItems && wishlistItems.length > 0) {
+      return <BookList books={wishlistItems} />;
     } else {
       return (
         <Alert>
@@ -83,7 +58,7 @@ function Wishlist() {
     <>
       <Banner title='wishlist' subtitle='favorites' />
       <div className='container my-5 py-5'>
-        <Render />
+        <RenderWishlist />
       </div>
     </>
   );
