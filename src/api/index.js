0889bcaf945956cfbf754478723225e270944644
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-05-25 13:15:28 +0200

    add token to api config

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,9 @@
-export const BASE_URL = `http://localhost:8080/api`
+export const BASE_URL = `http://localhost:8080/api`;
+
+let token = null;
+
+if (localStorage.getItem('token')) {
+  token = localStorage.getItem('token').replace(/"/gi, '');
+}
+
+export { token };
