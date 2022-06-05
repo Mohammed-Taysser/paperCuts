0190c6ce11cdbc01aec19556c68163d825186497
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-05 20:17:16 +0200

    replace auth context by token store

--- src/components/single/SingleBook.jsx
+++ src/components/single/SingleBook.jsx
@@ -1,55 +1,66 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import AddToWishList from '../AddToWishlist';
+import { useSelector } from 'react-redux';
 import { onImageNotLoad, Stars } from '../ManipulateData';
-import { Context as AuthContext } from '../../context/auth';
+import AddToWishList from '../AddToWishlist';
+import '../../assets/scss/components/singleBook.scss';
 
+/**
+ * @use `withWishlist` to enable add or remove from wishlist
+ * @use `md` to change md bootstrap grid system as media query
+ * @param {Object} props Object of setting (book, withWishlist, md)
+ * @returns  JSX
+ */
 function SingleBook(props) {
-  const { book, withWishlist } = props;
-  const auth_context = useContext(AuthContext);
+	const { book, withWishlist, md } = props;
+	const { token: reduxToken } = useSelector((state) => state['auth']);
 
-  return (
-    <div className={`col-6 col-md-${props.col} my-3`}>
-      <div className='card border-0 nice-shadow h-100 single-book'>
-        {auth_context.isAuth && withWishlist && (
-          <div
-            className='position-absolute top-0 end-0 m-2'
-            style={{ zIndex: 2 }}
-          >
-            <AddToWishList currentBook={book} />
-          </div>
-        )}
-        <div className='img'>
-          <img
-            src={book.image}
-            className='card-img-top'
-            onError={onImageNotLoad}
-            alt={book.title}
-          />
-        </div>
-        <div className='card-body'>
-          <h5 className='card-title'>
-            <Link to={`/books/${book.slug}`} className=''>
-              {book.title}
-            </Link>
-          </h5>
-          <h6 className='card-subtitle mb-3 text-muted'>
-            {typeof book.author === 'object' ? book.author.name : book.author}
-          </h6>
-          <h6 className='card-subtitle d-md-flex justify-content-between'>
-            <span className='text-aurora'>{book.price}$</span>
-            <span className='d-block d-md-inline'>
-              {<Stars stars_length={book.stars} />}
-            </span>
-          </h6>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`col-sm-6 col-md-${md} my-3`}>
+			<div className="card border-0 nice-shadow h-100 single-book">
+				{reduxToken && withWishlist && (
+					<div
+						className="position-absolute top-0 end-0 m-2"
+						style={{ zIndex: 2 }}
+					>
+						<AddToWishList currentBook={book} />
+					</div>
+				)}
+				<div className="img">
+					<img
+						src={book.image}
+						className="card-img-top"
+						onError={onImageNotLoad}
+						alt={book.title}
+					/>
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">
+						<Link to={`/books/${book.slug}`} className="">
+							{book.title}
+						</Link>
+					</h5>
+					<Link
+						className="text-muted h6"
+						to={`/authors/${book.author.username}`}
+					>
+						{book.author.name}
+					</Link>
+					<h6 className="d-md-flex justify-content-between mt-2">
+						<span className="text-aurora">{book.price}$</span>
+						<span className="d-block d-md-inline">
+							{<Stars stars_length={book.stars} />}
+						</span>
+					</h6>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 SingleBook.defaultProps = {
-  col: 3,
+	md: 3,
+	book: {},
 };
 
 export default SingleBook;
