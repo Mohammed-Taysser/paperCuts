71e3145f3ac049132295ef9f41bc91dbdf78298f
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-05 22:13:29 +0200

    replace auth context with auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,28 @@ import store from './redux/store';
 import Navbar from './layout/Navbar';
 import Footer from './layout/Footer';
 import MainRoute from './routes';
-import AuthContext from './context/auth';
 import BackToTop from './components/standalone/BackToTop';
+import AuthProvider from './provider/auth.provider';
 
 function App() {
-  useEffect(() => {
-    window.onload = function () {
-      document.body.classList.remove('load');
-    };
-  }, []);
+	useEffect(() => {
+		window.onload = function () {
+			document.body.classList.remove('load');
+		};
+	}, []);
 
-  return (
-    <Provider store={store}>
-      <AuthContext>
-        <BrowserRouter>
-          <Navbar />
-          <BackToTop />
-          <MainRoute />
-          <Footer />
-        </BrowserRouter>
-      </AuthContext>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<AuthProvider>
+				<BrowserRouter>
+					<Navbar />
+					<BackToTop />
+					<MainRoute />
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
+		</Provider>
+	);
 }
 
 export default App;
