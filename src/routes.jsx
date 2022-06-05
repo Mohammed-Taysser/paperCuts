668d22cbaabaa42094ad0f1d207cb4b82557728e
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-05 22:13:59 +0200

    refactor with token

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,71 +1,69 @@
-import React, { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Context as AuthContext } from './context/auth';
-
-import Homepage from './pages/noAuth/homepage';
-import PageNotFound from './pages/noAuth/404';
-import AboutUs from './pages/noAuth/about-us';
-import ContactUs from './pages/noAuth/contact-us';
-import Login from './pages/noAuth/login';
-import Register from './pages/noAuth/register';
-import ForgetPassword from './pages/noAuth/forget-password';
-import Profile from './pages/needAuth/profile';
-import Wishlist from './pages/needAuth/wishlist';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import CreateBook from './dashboard/createBook';
+import UpdateBook from './dashboard/updateBook';
 import Cart from './pages/needAuth/cart';
 import Checkout from './pages/needAuth/checkout';
-import Books from './pages/noAuth/books';
+import OrderDetails from './pages/needAuth/orderDetails';
+import Order from './pages/needAuth/orders';
+import Profile from './pages/needAuth/profile';
+import Wishlist from './pages/needAuth/wishlist';
+import PageNotFound from './pages/noAuth/404';
+import AboutUs from './pages/noAuth/about-us';
+import AuthorDetails from './pages/noAuth/authorDetails';
+import Author from './pages/noAuth/authors';
 import BooksDetails from './pages/noAuth/bookDetails';
+import Books from './pages/noAuth/books';
 import Category from './pages/noAuth/category';
 import CategoryDetails from './pages/noAuth/categoryDetails';
-import Events from './pages/noAuth/events';
+import ContactUs from './pages/noAuth/contact-us';
 import EventsDetails from './pages/noAuth/eventDetails';
-import Author from './pages/noAuth/authors';
-import AuthorDetails from './pages/noAuth/authorDetails';
-import Order from './pages/needAuth/orders';
-import OrderDetails from './pages/needAuth/orderDetails';
-import WebsitePages from './pages/noAuth/pages';
-import CreateBook from './pages/needAuth/create/createBook';
+import Events from './pages/noAuth/events';
+import Homepage from './pages/noAuth/homepage';
+import Login from './pages/noAuth/login';
+import Register from './pages/noAuth/register';
 
 function PaperCutsRoutes() {
-  const auth_context = useContext(AuthContext);
-  const auth_routes = () => {
-    if (auth_context.isAuth) {
-      return (
-        <>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/wishlist' element={<Wishlist />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/orders' element={<Order />} />
-          <Route path='/orders/:id' element={<OrderDetails />} />
-          <Route path='/create/book' element={<CreateBook />} />
-        </>
-      );
-    }
-  };
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='*' element={<PageNotFound />} />
-        <Route path='/pages' element={<WebsitePages />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/about-us' element={<AboutUs />} />
-        <Route path='/contact-us' element={<ContactUs />} />
-        {auth_routes()}
-        <Route path='/books' element={<Books />} />
-        <Route path='/books/:slug' element={<BooksDetails />} />
-        <Route path='/category' element={<Category />} />
-        <Route path='/category/:slug' element={<CategoryDetails />} />
-        <Route path='/events/' element={<Events />} />
-        <Route path='/events/:slug' element={<EventsDetails />} />
-        <Route path='/authors' element={<Author />} />
-        <Route path='/authors/:username' element={<AuthorDetails />} />
-        <Route path='/forget-password' element={<ForgetPassword />} />
-      </Routes>
-    </>
-  );
+	const { token } = useSelector((state) => state['auth']);
+
+	const auth_routes = () => {
+		if (token) {
+			return (
+				<>
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/wishlist" element={<Wishlist />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/orders" element={<Order />} />
+					<Route path="/orders/:id" element={<OrderDetails />} />
+					<Route path="/create/book" element={<CreateBook />} />
+					<Route path="/update/book/:slug" element={<UpdateBook />} />
+				</>
+			);
+		}
+	};
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="*" element={<PageNotFound />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/about-us" element={<AboutUs />} />
+				<Route path="/contact-us" element={<ContactUs />} />
+				{auth_routes()}
+				<Route path="/books" element={<Books />} />
+				<Route path="/books/:slug" element={<BooksDetails />} />
+				<Route path="/category" element={<Category />} />
+				<Route path="/category/:slug" element={<CategoryDetails />} />
+				<Route path="/events/" element={<Events />} />
+				<Route path="/events/:slug" element={<EventsDetails />} />
+				<Route path="/authors" element={<Author />} />
+				<Route path="/authors/:username" element={<AuthorDetails />} />
+			</Routes>
+		</>
+	);
 }
 
 export default PaperCutsRoutes;
