188e8a905972dc536694defdda23858b7e40ee26
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-05 20:28:47 +0200

    refactor temporally token value

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
+// axios BASE_URL for any api call
 export const BASE_URL = `http://localhost:8080/api`;
 
+// token for access authorization api call
 let token = null;
 
-if (localStorage.getItem('token')) {
-  token = localStorage.getItem('token').replace(/"/gi, '');
+if (localStorage.getItem('token') !== null) {
+	token = JSON.parse(localStorage.getItem('token'));
 }
 
 export { token };
