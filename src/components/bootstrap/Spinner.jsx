04e1ff7874a099f3a8b7b6c46eee678bc2ea0dae
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-05 20:27:16 +0200

    create loading button

--- src/components/bootstrap/Spinner.jsx
+++ src/components/bootstrap/Spinner.jsx
@@ -2,21 +2,41 @@ import React from 'react';
 import '../../assets/scss/components/spinner.scss';
 
 const Spinner = (props) => {
-  const spinnerClassName = `spinner-border text-${props.color} ${
-    props.sm ? 'spinner-border-sm' : ''
-  }`;
-  return (
-    <div className={`text-center ${props.className}`}>
-      <div className={spinnerClassName} role='status'>
-        <span className='visually-hidden'>{props.text}</span>
-      </div>
-      {props.text && <p className=''>{props.text}</p>}
-    </div>
-  );
+	const spinnerClassName = `spinner-border text-${props.color} ${
+		props.sm ? 'spinner-border-sm' : ''
+	}`;
+	return (
+		<div className={`text-center ${props.className}`}>
+			<div className={spinnerClassName} role="status">
+				<span className="visually-hidden">{props.text}</span>
+			</div>
+			{props.text && <p className="">{props.text}</p>}
+		</div>
+	);
 };
 
 Spinner.defaultProps = {
-  color: 'aurora',
+	color: 'aurora',
+};
+
+function LoadingButton(props) {
+	const { sm, color, className } = props;
+
+	return (
+		<button className={`btn btn-${color} ${className}`} type="button" disabled>
+			<span
+				className={`spinner-border ${sm ? 'spinner-border-sm' : ''}`}
+				role="status"
+				aria-hidden="true"
+			></span>
+			<span className="visually-hidden">Loading...</span>
+		</button>
+	);
+}
+
+LoadingButton.defaultProps = {
+	color: 'aurora',
 };
 
 export default Spinner;
+export { LoadingButton };
