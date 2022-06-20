1d85a955064b236cfeb3eabd66620dfc613ce611
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-20 12:19:25 +0200

    move redux provider to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import Navbar from './layout/Navbar';
-import Footer from './layout/Footer';
 import MainRoute from './routes';
-import BackToTop from './components/standalone/BackToTop';
-import AuthProvider from './provider/auth.provider';
 
 function App() {
 	useEffect(() => {
@@ -16,16 +10,9 @@ function App() {
 	}, []);
 
 	return (
-		<Provider store={store}>
-			<AuthProvider>
-				<BrowserRouter>
-					<Navbar />
-					<BackToTop />
-					<MainRoute />
-					<Footer />
-				</BrowserRouter>
-			</AuthProvider>
-		</Provider>
+		<BrowserRouter>
+			<MainRoute />
+		</BrowserRouter>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
+import AuthProvider from './provider/auth.provider';
 import reportWebVitals from './__tests__/reportWebVitals';
-import './assets/scss/core.scss';
+import './assets/scss/core.paperCuts.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
