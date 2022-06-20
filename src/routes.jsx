9201389d7b90382928a3a74ffb4e6d877098f5fc
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-06-20 12:18:50 +0200

    update auth routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import CreateBook from './dashboard/createBook';
-import UpdateBook from './dashboard/updateBook';
-import Cart from './pages/needAuth/cart';
-import Checkout from './pages/needAuth/checkout';
-import OrderDetails from './pages/needAuth/orderDetails';
-import Order from './pages/needAuth/orders';
-import Profile from './pages/needAuth/profile';
-import Wishlist from './pages/needAuth/wishlist';
+
 import PageNotFound from './pages/noAuth/404';
 import AboutUs from './pages/noAuth/about-us';
 import AuthorDetails from './pages/noAuth/authorDetails';
@@ -24,45 +17,61 @@ import Homepage from './pages/noAuth/homepage';
 import Login from './pages/noAuth/login';
 import Register from './pages/noAuth/register';
 
-function PaperCutsRoutes() {
-	const { token } = useSelector((state) => state['auth']);
+import Cart from './pages/needAuth/cart';
+import Checkout from './pages/needAuth/checkout';
+import OrderDetails from './pages/needAuth/orderDetails';
+import Order from './pages/needAuth/orders';
+import Profile from './pages/needAuth/profile';
+import Wishlist from './pages/needAuth/wishlist';
 
-	const auth_routes = () => {
-		if (token) {
+import PaperCutsLayout from './layout/PaperCuts.layout';
+
+function PaperCutsRoutes() {
+	const { token, jwt_token } = useSelector((state) => state['auth']);
+
+	const AuthRoutes = () => {
+		if (token && ['user', 'author', 'admin'].includes(jwt_token?.role)) {
 			return (
 				<>
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="/wishlist" element={<Wishlist />} />
-					<Route path="/checkout" element={<Checkout />} />
-					<Route path="/orders" element={<Order />} />
-					<Route path="/orders/:id" element={<OrderDetails />} />
-					<Route path="/create/book" element={<CreateBook />} />
-					<Route path="/update/book/:slug" element={<UpdateBook />} />
+					<Route path="profile" element={<Profile />} />
+					<Route path="cart" element={<Cart />} />
+					<Route path="wishlist" element={<Wishlist />} />
+					<Route path="checkout" element={<Checkout />} />
+					<Route path="orders" element={<Order />} />
+					<Route path="orders/:id" element={<OrderDetails />} />
 				</>
 			);
 		}
 	};
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Homepage />} />
+
+	const MainRoutes = () => {
+		return (
+			<>
+				<Route index element={<Homepage />} />
 				<Route path="*" element={<PageNotFound />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/about-us" element={<AboutUs />} />
-				<Route path="/contact-us" element={<ContactUs />} />
-				{auth_routes()}
-				<Route path="/books" element={<Books />} />
-				<Route path="/books/:slug" element={<BooksDetails />} />
-				<Route path="/category" element={<Category />} />
-				<Route path="/category/:slug" element={<CategoryDetails />} />
-				<Route path="/events/" element={<Events />} />
-				<Route path="/events/:slug" element={<EventsDetails />} />
-				<Route path="/authors" element={<Author />} />
-				<Route path="/authors/:username" element={<AuthorDetails />} />
-			</Routes>
-		</>
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+				<Route path="about-us" element={<AboutUs />} />
+				<Route path="contact-us" element={<ContactUs />} />
+				<Route path="books" element={<Books />} />
+				<Route path="books/:slug" element={<BooksDetails />} />
+				<Route path="category" element={<Category />} />
+				<Route path="category/:slug" element={<CategoryDetails />} />
+				<Route path="events/" element={<Events />} />
+				<Route path="events/:slug" element={<EventsDetails />} />
+				<Route path="authors" element={<Author />} />
+				<Route path="authors/:username" element={<AuthorDetails />} />
+			</>
+		);
+	};
+
+	return (
+		<Routes>
+			<Route path="/" element={<PaperCutsLayout />}>
+				{MainRoutes()}
+				{AuthRoutes()}
+			</Route>
+		</Routes>
 	);
 }
 
