3b3934fa4f4937ef8834ad029e6fea120dd9ea1b
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-06 23:23:46 +0200

    temp: comment dashboard routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,14 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import authRoutes from './authRoutes';
 import publicRoutes from './publicRoutes';
-import dashboardRoutes from './dashboardRoutes';
+// import dashboardRoutes from './dashboardRoutes';
 
 import PaperCutsLayout from '../layout/PaperCuts.layout';
 
 function mainRoutes() {
 	return (
 		<Routes>
-			<Route path="/">{dashboardRoutes()}</Route>
+			{/* <Route path="/">{dashboardRoutes()}</Route> */}
 			<Route path="/" element={<PaperCutsLayout />}>
 				{authRoutes()}
 				{publicRoutes()}
