e393c8c1636caec5a7872c95514a23e8d409a5c3
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-06 23:48:08 +0200

    fix old redux `jwt_token` variable with user variable

--- src/components/BillingForm.jsx
+++ src/components/BillingForm.jsx
@@ -5,74 +5,16 @@ import checkoutValidate from '../validations/checkout.validate';
 import { LoadingButton } from './bootstrap/Spinner';
 
 function BillingForm(props) {
-	const { jwt_token } = useSelector((state) => state['auth']);
+	const { user } = useSelector((state) => state['auth']);
 	const { loading, errors, setErrors } = props;
 
 	const [formData, setFormData] = useState({
-		fullName: jwt_token.username || '',
+		fullName: user.username || '',
 		country: '',
 		address: '',
 		phone: '',
 		note: '',
 	});
-
-	// const PaymentMethod = () => {
-	//   return (
-	//     <div className='mt-4'>
-	//       <RadioField
-	//         name='paymentMethod'
-	//         id='payment-method-cash'
-	//         value='cash'
-	//         onChange={onInputChange}
-	//         label={
-	//           <>
-	//             <FcInTransit className='h4 my-0 mx-2 ' />
-	//             Cash on delivery
-	//           </>
-	//         }
-	//         checked={formData['paymentMethod'] === 'cash' ? true : false}
-	//       />
-	//       <RadioField
-	//         name='paymentMethod'
-	//         id='payment-method-master-card'
-	//         value='masterCard'
-	//         onChange={onInputChange}
-	//         label={
-	//           <>
-	//             <FaCcMastercard className='h4 my-0 mx-2 ' />
-	//             master card
-	//           </>
-	//         }
-	//         checked={formData['paymentMethod'] === 'masterCard' ? true : false}
-	//       />
-	//       <RadioField
-	//         name='paymentMethod'
-	//         id='payment-method-visa'
-	//         value='visa'
-	//         onChange={onInputChange}
-	//         label={
-	//           <>
-	//             <RiVisaFill className='h4 my-0 mx-2 ' />
-	//             visa
-	//           </>
-	//         }
-	//         checked={formData['paymentMethod'] === 'visa' ? true : false}
-	//       />
-	//       <RadioField
-	//         name='paymentMethod'
-	//         id='payment-method-paypal'
-	//         value='paypal'
-	//         onChange={onInputChange}
-	//         label={
-	//           <>
-	//             <BsPaypal className='h4 my-0 mx-2 ' />
-	//             paypal
-	//           </>
-	//         }
-	//         checked={formData['paymentMethod'] === 'paypal' ? true : false}
-	//       />
-	//   );
-	// };
 
 	const onInputChange = (evt) => {
 		setFormData({ ...formData, [evt.target.name]: evt.target.value });
@@ -80,9 +22,9 @@ function BillingForm(props) {
 
 	const onFormSubmit = (evt) => {
 		evt.preventDefault();
-		setErrors(null);		
+		setErrors(null);
 		const errorsAsObject = checkoutValidate(formData);
-		
+
 		if (Object.keys(errorsAsObject).length === 0) {
 			props.onFormSubmit(formData);
 		} else {
