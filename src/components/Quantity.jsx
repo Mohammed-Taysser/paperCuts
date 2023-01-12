38230a0e629c51093565b1868facda99bc84be27
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-12 21:13:59 +0200

    refactor & reformat & remove old style files

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -3,64 +3,64 @@ import { FaMinus, FaPlus } from 'react-icons/fa';
 import Spinner from './bootstrap/Spinner';
 
 function QuantityControlButton(props) {
-  const { loading, initQuantity = 1 } = props;
-  const [quantityNumber, setQuantityNumber] = useState(initQuantity);
+	const { loading, initQuantity = 1 } = props;
+	const [quantityNumber, setQuantityNumber] = useState(initQuantity);
 
-  const onQuantityAdd = (evt) => {
-    evt.preventDefault();
-    if (quantityNumber >= 1) {
-      let new_value = quantityNumber + 1;
-      setQuantityNumber(new_value);
-      props.onQuantityChange(new_value);
-    }
-  };
+	const onQuantityAdd = (evt) => {
+		evt.preventDefault();
+		if (quantityNumber >= 1) {
+			let new_value = quantityNumber + 1;
+			setQuantityNumber(new_value);
+			props.onQuantityChange(new_value);
+		}
+	};
 
-  const onQuantityMinus = (evt) => {
-    evt.preventDefault();
-    if (quantityNumber > 1) {
-      let new_value = quantityNumber - 1;
-      setQuantityNumber(new_value);
-      props.onQuantityChange(new_value);
-    }
-  };
+	const onQuantityMinus = (evt) => {
+		evt.preventDefault();
+		if (quantityNumber > 1) {
+			let new_value = quantityNumber - 1;
+			setQuantityNumber(new_value);
+			props.onQuantityChange(new_value);
+		}
+	};
 
-  if (loading) {
-    return <Spinner />;
-  }
+	if (loading) {
+		return <Spinner />;
+	}
 
-  return (
-    <div className='d-flex align-items-center qty-container'>
-      <button
-        className='btn-qty btn-qty-down'
-        onClick={onQuantityAdd}
-        disabled={props.disabled}
-      >
-        <FaPlus />
-      </button>
-      <input
-        className='form-qty'
-        type='number'
-        name='quantity'
-        value={quantityNumber}
-        readOnly
-      />
-      <button
-        className='btn-qty btn-qty-up'
-        onClick={onQuantityMinus}
-        disabled={props.disabled}
-      >
-        <FaMinus />
-      </button>
-    </div>
-  );
+	return (
+		<div className='d-flex align-items-center qty-container'>
+			<button
+				className='btn-qty btn-qty-down'
+				onClick={onQuantityAdd}
+				disabled={props.disabled}
+			>
+				<FaPlus />
+			</button>
+			<input
+				className='form-qty'
+				type='number'
+				name='quantity'
+				value={quantityNumber}
+				readOnly
+			/>
+			<button
+				className='btn-qty btn-qty-up'
+				onClick={onQuantityMinus}
+				disabled={props.disabled}
+			>
+				<FaMinus />
+			</button>
+		</div>
+	);
 }
 
 QuantityControlButton.defaultProps = {
-  initQuantity: 1,
-  disabled: false,
-  onQuantityChange: (data) => {
-    console.log(data);
-  },
+	initQuantity: 1,
+	disabled: false,
+	onQuantityChange: (data) => {
+		console.log(data);
+	},
 };
 
 export default QuantityControlButton;
