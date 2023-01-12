67846aea876401e5e412a1397af08ef831aa4a5b
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-12 21:11:57 +0200

    refactor & reformat

--- src/components/standalone/BookList.jsx
+++ src/components/standalone/BookList.jsx
@@ -3,26 +3,26 @@ import Alert from '../bootstrap/Alert';
 import SingleBook from '../single/SingleBook';
 
 function BookList(props) {
-  const { books, outerClass } = props;
-  if (books && books.length > 0) {
-    let books_list = books.map((book, index) => (
-      <SingleBook book={book} key={index} />
-    ));
-    return (
-      <div
-        className={`row justify-content-center align-items-center align-items-stretch ${outerClass}`}
-      >
-        {books_list}
-      </div>
-    );
-  } else {
-    return <Alert>no available books found</Alert>;
-  }
+	const { books, outerClass } = props;
+	if (books && books.length > 0) {
+		let books_list = books.map((book) => (
+			<SingleBook book={book} key={book._id} />
+		));
+		return (
+			<div
+				className={`row justify-content-center align-items-center align-items-stretch ${outerClass}`}
+			>
+				{books_list}
+			</div>
+		);
+	} else {
+		return <Alert>no available books found</Alert>;
+	}
 }
 
 BookList.defaultProps = {
-  books: [],
-  outerClass: '',
+	books: [],
+	outerClass: '',
 };
 
 export default BookList;
