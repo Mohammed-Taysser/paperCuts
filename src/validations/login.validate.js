5bd501ae65a081567aab590c36f3e7594abf773f
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-12 20:46:15 +0200

    re-format register & login pages

--- src/validations/login.validate.js
+++ src/validations/login.validate.js
@@ -1,27 +1,27 @@
 import validator from 'validator';
 
 const loginValidate = (formData) => {
-  const { email, password } = formData;
+	const { email, password } = formData;
 
-  let errorAsObject = {};
+	let errorAsObject = {};
 
-  if (email) {
-    if (!validator.isEmail(email)) {
-      errorAsObject.email = `please provide valid email`;
-    }
-  } else {
-    errorAsObject.email = `email is missing`;
-  }
+	if (email) {
+		if (!validator.isEmail(email)) {
+			errorAsObject.email = `please provide valid email`;
+		}
+	} else {
+		errorAsObject.email = `email is missing`;
+	}
 
-  if (password) {
-    if (!validator.isStrongPassword(password)) {
-      errorAsObject.password = `please provide strong password`;
-    }
-  } else {
-    errorAsObject.password = `password is missing`;
-  }
+	if (password) {
+		if (!validator.isStrongPassword(password)) {
+			errorAsObject.password = `please provide strong password`;
+		}
+	} else {
+		errorAsObject.password = `password is missing`;
+	}
 
-  return errorAsObject;
+	return errorAsObject;
 };
 
 export default loginValidate;
