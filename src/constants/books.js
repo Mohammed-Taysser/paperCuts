051232a25063ac87a2876d9a4053bc969621335f
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-12 20:53:01 +0200

    move soty type array to single file

---
+++ src/constants/books.js
@@ -0,0 +1,24 @@
+const SELECT_SORT_TYPES = [
+	{
+		label: 'choose sort type',
+		value: '',
+	},
+	{
+		label: 'sort by latest',
+		value: 'date',
+	},
+	{
+		label: 'sort by average rating',
+		value: 'stars',
+	},
+	{
+		label: 'sort by price low to height',
+		value: 'price',
+	},
+	{
+		label: 'sort by price height to low',
+		value: 'price-desc',
+	},
+];
+
+export default SELECT_SORT_TYPES;
