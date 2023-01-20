12164eb29ce4bc01608ea2cfa0ea498bbc2648c3
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-20 10:21:15 +0200

    refactor cart manipulate

--- src/components/book-details/AddToCart.jsx
+++ src/components/book-details/AddToCart.jsx
@@ -1,177 +1,53 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
-import { BiEdit } from 'react-icons/bi';
-import { BsCartDash, BsCartPlus } from 'react-icons/bs';
+import React, { useEffect, useState } from 'react';
+import { BsCartPlus } from 'react-icons/bs';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
-	createCartItem,
-	deleteCartItem,
-	getCartByBookId,
-	updateCartQuantity,
-} from '../../api/cart.api';
+	addCartItem,
+	fetchCartItemByBookId,
+} from '../../redux/features/cart.slice';
 import Quantity from '../Quantity';
 import Alert from '../bootstrap/Alert';
 import Spinner from '../bootstrap/Spinner';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCartItemByBookId } from '../../redux/features/cart.slice';
 
 function AddToCart(props) {
 	const { currentBook } = props;
 	const dispatch = useDispatch();
 	const cartState = useSelector((state) => state['cart']['single']);
-	// const [loading, setLoading] = useState(true);
 	const [quantity, setQuantity] = useState(1);
-	const [message, setMessage] = useState(null);
-	// const [currentCart, setCurrentCart] = useState(null);
 
-	useLayoutEffect(() => {
-		dispatch(fetchCartItemByBookId({id:currentBook._id}));
+	useEffect(() => {
+		dispatch(fetchCartItemByBookId({ id: currentBook._id }));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	useEffect(() => {
-		if(cartState.item){onCartItemsLoad(cartState.item)}
-	
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [cartState.item])
-	
-
-
-	const onCartItemsLoad = (response_data) => {
-		// setCurrentCart(response_data);
-		if (response_data) {
-			setQuantity(response_data.quantity);
-			setMessageByType('success', 'In cart');
-		}
-	};
-
-	const setMessageByType = (type, label = '') => {
-		switch (type) {
-			case 'success':
-				setMessage({
-					type: 'success',
-					label: <>{label}</>,
-				});
-				break;
-			case 'error':
-				setMessage({
-					type: 'danger',
-					label: <>{label}</>,
-				});
-				break;
-
-			case 'warn':
-				setMessage({
-					type: 'warning',
-					label: <>{label}</>,
-				});
-				break;
-			default:
-				break;
-		}
-	};
-
 	const onAddToCartClick = () => {
-		// setLoading(true);
 		let newBookData = { quantity, ...currentBook, bookId: currentBook._id };
 		delete newBookData._id;
-		createCartItem(newBookData)
-			.then((response) => {
-				// setCurrentCart(response.data);
-				setMessageByType(
-					'success',
-					<>
-						successfully added to{' '}
-						<Link to='/cart' className='alert-link'>
-							cart
-						</Link>
-					</>
-				);
-			})
-			.catch((error) => {
-				setMessageByType('error', error.message);
-			})
-			.finally(() => {
-				// setLoading(false);
-			});
-	};
-
-	const onUpdateCartBook = () => {
-		// if (quantity !== currentCart.quantity) {
-		// 	setLoading(true);
-		// 	updateCartQuantity(currentCart._id, quantity)
-		// 		.then((response) => {
-		// 			setCurrentCart(response.data);
-		// 			setMessageByType('success', 'successfully updated');
-		// 		})
-		// 		.catch((error) => {
-		// 			setMessageByType('error', error.message);
-		// 		})
-		// 		.finally(() => {
-		// 			setLoading(false);
-		// 		});
-		// } else {
-		// 	setMessageByType('warn', 'no change');
-		// }
-	};
-
-	const onRemoveBtnClick = () => {
-		// setLoading(true);
-
-		deleteCartItem(cartState.item._id)
-			.then(() => {
-				setMessageByType('success', ' Success: Removed From Cart');
-				// setCurrentCart(null);
-				setQuantity(1);
-			})
-			.catch((error) => {
-				setMessageByType('error', error.message);
-			})
-			.finally(() => {
-				// setLoading(false);
-			});
-	};
-
-	const RenderAddToCartButton = () => {
-		if (cartState.item) {
-			return (
-				<>
-					<button className='btn btn-aurora mx-4' onClick={onUpdateCartBook}>
-						<BiEdit className='h5 m-0' /> Save
-					</button>
-					<button
-						className='btn btn-outline-danger'
-						type='button'
-						onClick={onRemoveBtnClick}
-					>
-						<BsCartDash className='h5 m-0' /> Remove
-					</button>
-				</>
-			);
-		} else {
-			return (
-				<button className='btn btn-aurora mx-4' onClick={onAddToCartClick}>
-					<BsCartPlus className='h5 m-0' /> Add To Cart
-				</button>
-			);
-		}
+		dispatch(addCartItem({ data: newBookData }));
 	};
 
 	if (cartState.loading) {
 		return <Spinner />;
-	}else if(cartState.error){
-	return <Alert>{JSON.stringify(cartState.error)}</Alert>} 
-	
-	else {
+	} else if (cartState.error) {
+		return <Alert>{JSON.stringify(cartState.error)}</Alert>;
+	} else if (cartState.item) {
+		return (
+			<Alert sm color='success'>
+				already added to{' '}
+				<Link to='/cart' className='alert-link'>
+					cart
+				</Link>
+			</Alert>
+		);
+	} else {
 		return (
 			<form onSubmit={(evt) => evt.preventDefault()}>
-				{message && (
-					<Alert sm color={message.type}>
-						{message.label}
-					</Alert>
-				)}
 				<div className='d-flex my-3'>
 					<Quantity onQuantityChange={setQuantity} initQuantity={quantity} />
-					<RenderAddToCartButton />
+					<button className='btn btn-aurora mx-4' onClick={onAddToCartClick}>
+						<BsCartPlus className='h5 m-0' /> Add To Cart
+					</button>
 				</div>
 			</form>
 		);
--- src/redux/features/cart.slice.js
+++ src/redux/features/cart.slice.js
@@ -1,30 +1,302 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const fetchAllCartItems = createAsyncThunk('cart/all', async () => {
+	try {
+		const response = await axios.get(`/cart/`);
+		return {
+			status: 'success',
+			items: response.data,
+		};
+	} catch (error) {
+		return {
+			status: 'error',
+			error: error.response.data.error,
+		};
+	}
+});
+
+const allCartExtraReducers = () => {
+	const { pending, fulfilled, rejected } = fetchAllCartItems;
+
+	return {
+		[pending]: (state) => {
+			state.loading = true;
+			state.items = [];
+		},
+		[fulfilled]: (state, action) => {
+			state.loading = false;
+			if (action.payload.status === 'success') {
+				const subtotal = calculateAmount(action.payload.items);
+				state.items = action.payload.items;
+				state.subtotal = subtotal;
+			} else {
+				state.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.loading = false;
+			state.error = action.error;
+		},
+	};
+};
+
+const fetchCartItemByBookId = createAsyncThunk('cart/view', async ({ id }) => {
+	try {
+		const response = await axios.get(`/cart/view/${id}`);
+		return {
+			status: 'success',
+			item: response.data,
+		};
+	} catch (error) {
+		return {
+			status: 'error',
+			error: error.response.data.error,
+		};
+	}
+});
+
+const fetchCartByBookIdExtraReducers = () => {
+	const { pending, fulfilled, rejected } = fetchCartItemByBookId;
+
+	return {
+		[pending]: (state) => {
+			state.single.loading = true;
+			state.single.item = [];
+		},
+		[fulfilled]: (state, action) => {
+			state.single.loading = false;
+			if (action.payload.status === 'success') {
+				state.single.item = action.payload.item;
+			} else {
+				state.single.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.single.loading = false;
+			state.single.error = action.error;
+		},
+	};
+};
+
+const fetchCouponByLabel = createAsyncThunk(
+	'coupon/search',
+	async ({ label }) => {
+		try {
+			const response = await axios.get(`/coupon/view/${label}`);
+			return {
+				status: 'success',
+				item: response.data,
+			};
+		} catch (error) {
+			return {
+				status: 'error',
+				error: error.response.data.error,
+			};
+		}
+	}
+);
+
+const couponSearchExtraReducers = () => {
+	const { pending, fulfilled, rejected } = fetchCouponByLabel;
+
+	return {
+		[pending]: (state) => {
+			state.coupons.loading = true;
+		},
+		[fulfilled]: (state, action) => {
+			state.coupons.loading = false;
+			if (action.payload.status === 'success') {
+				state.coupons.searched = action.payload.item;
+			} else {
+				state.coupons.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.coupons.loading = false;
+			state.coupons.error = action.error;
+		},
+	};
+};
+
+const addCartItem = createAsyncThunk('cart/add', async ({ data }) => {
+	try {
+		const response = await axios.post(`/cart/create`, data);
+		return {
+			status: 'success',
+			item: response.data,
+		};
+	} catch (error) {
+		return {
+			status: 'error',
+			error: error.response.data.error,
+		};
+	}
+});
+
+const addCartItemExtraReducers = () => {
+	const { pending, fulfilled, rejected } = addCartItem;
+
+	return {
+		[pending]: (state) => {
+			state.single.loading = true;
+			state.single.item = [];
+		},
+		[fulfilled]: (state, action) => {
+			state.single.loading = false;
+			if (action.payload.status === 'success') {
+				state.single.item = action.payload.item;
+			} else {
+				state.single.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.single.loading = false;
+			state.single.error = action.error;
+		},
+	};
+};
+
+const updateCartItem = createAsyncThunk(
+	'cart/update',
+	async ({ cartId, quantity }) => {
+		try {
+			const response = await axios.patch(`/cart/update/${cartId}`, {
+				quantity,
+			});
+			return {
+				status: 'success',
+				item: response.data,
+			};
+		} catch (error) {
+			return {
+				status: 'error',
+				error: error.response.data.error,
+			};
+		}
+	}
+);
+
+const updateCartItemExtraReducers = () => {
+	const { pending, fulfilled, rejected } = updateCartItem;
+
+	return {
+		[pending]: (state) => {
+			state.loading = true;
+		},
+		[fulfilled]: (state, action) => {
+			state.loading = false;
+			if (action.payload.status === 'success') {
+				const updatedCartItems = state.items.map((item) =>
+					item._id === action.payload.item._id
+						? { ...item, quantity: action.payload.item.quantity }
+						: item
+				);
+				const subtotal = calculateAmount(updatedCartItems);
+				state.items = updatedCartItems;
+				state.subtotal = subtotal;
+			} else {
+				state.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.loading = false;
+			state.error = action.error;
+		},
+	};
+};
+
+const deleteCartItem = createAsyncThunk('cart/delete', async ({ cartId }) => {
+	try {
+		const response = await axios.delete(`/cart/delete/${cartId}`);
+		return {
+			status: 'success',
+			item: response.data,
+		};
+	} catch (error) {
+		return {
+			status: 'error',
+			error: error.response.data.error,
+		};
+	}
+});
+
+const deleteCartItemExtraReducers = () => {
+	const { pending, fulfilled, rejected } = deleteCartItem;
+
+	return {
+		[pending]: (state) => {
+			state.loading = true;
+		},
+		[fulfilled]: (state, action) => {
+			state.loading = false;
+			if (action.payload.status === 'success') {
+				const updatedCartItems = state.items.filter(
+					(item) => item._id !== action.payload.item._id
+				);
+				const subtotal = calculateAmount(updatedCartItems);
+				state.items = updatedCartItems;
+				state.subtotal = subtotal;
+			} else {
+				state.error = action.payload.error;
+			}
+		},
+		[rejected]: (state, action) => {
+			state.loading = false;
+			state.error = action.error;
+		},
+	};
+};
+
+function calculateAmount(items) {
+	return items.reduce((total, item) => {
+		total += item.price * item.quantity;
+		return total;
+	}, 0);
+}
 
 export const couponSlice = createSlice({
 	name: 'cart',
 	initialState: {
 		discount: 0,
 		shipping: 50,
-		coupons: [],
-		total: 0,
+		coupons: {
+			searched: {},
+			applied: [],
+			error: '',
+			loading: false,
+		},
+		subtotal: 0,
+		error: null,
+		loading: false,
+		items: [],
+		single: {
+			error: null,
+			loading: false,
+			item: null,
+		},
 	},
 	reducers: {
-		calculateAmount: (state, action) => {
-			state.total = action.payload.reduce((total, item) => {
-				total += item.price * item.quantity;
-				return total;
-			}, 0);
-		},
 		addCoupon: (state, action) => {
-			state.coupons.push(action.payload);
-			state.discount = reduceCoupons(state.coupons);
+			state.coupons.applied.push(action.payload);
+			state.discount = reduceCoupons(state.coupons.applied);
+			state.subtotal = calculateAmount(state.items);
 		},
 		removeCoupon: (state, action) => {
-			state.coupons = state.coupons.filter(
+			state.coupons.applied = state.coupons.applied.filter(
 				(coupon) => coupon._id !== action.payload
 			);
-			state.discount = reduceCoupons(state.coupons);
+			state.discount = reduceCoupons(state.coupons.applied);
+			state.subtotal = calculateAmount(state.items);
 		},
+	},
+	extraReducers: {
+		...allCartExtraReducers(),
+		...addCartItemExtraReducers(),
+		...updateCartItemExtraReducers(),
+		...deleteCartItemExtraReducers(),
+		...couponSearchExtraReducers(),
+		...fetchCartByBookIdExtraReducers(),
 	},
 });
 
@@ -36,6 +308,13 @@ function reduceCoupons(coupons) {
 }
 
 // Action creators are generated for each case reducer function
-export const { calculateAmount, addCoupon, removeCoupon } = couponSlice.actions;
-
+export const { addCoupon, removeCoupon } = couponSlice.actions;
+export {
+	fetchAllCartItems,
+	addCartItem,
+	updateCartItem,
+	deleteCartItem,
+	fetchCouponByLabel,
+	fetchCartItemByBookId,
+};
 export default couponSlice.reducer;
