2c9bce43b5a55f31fbb6f580b2c543573f2f3bdb
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-01-20 09:29:17 +0200

    refactor `wishlist` page

--- src/pages/needAuth/wishlist.jsx
+++ src/pages/needAuth/wishlist.jsx
@@ -1,67 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getAllAuthorWishlist } from '../../api/wishlist.api';
+import Alert from '../../components/bootstrap/Alert';
 import { RowOfPlaceholderCard } from '../../components/bootstrap/Placeholder';
 import Banner from '../../components/standalone/Banner';
-import Alert from '../../components/bootstrap/Alert';
-import usePageTitle from '../../hooks/usePageTitle';
 import BookList from '../../components/standalone/BookList';
+import usePageTitle from '../../hooks/usePageTitle';
+import { fetchAllWishlist } from '../../redux/features/wishlist.slice';
 
 function Wishlist() {
-  usePageTitle('Wishlist');
-  const [wishlistItems, setWishlistItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [loadingError, setLoadingError] = useState(null);
+	usePageTitle('Wishlist');
+	const dispatch = useDispatch();
+	const wishlistState = useSelector((state) => state['wishlist']['all']);
 
-  useEffect(() => {
-    api_get_wishlist();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		dispatch(fetchAllWishlist());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  const api_get_wishlist = () => {
-    getAllAuthorWishlist()
-      .then((response) => {
-        setWishlistItems(response.data);
-      })
-      .catch((error) => {
-        setLoadingError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+	const RenderWishlist = () => {
+		if (wishlistState.loading) {
+			return <RowOfPlaceholderCard num={6} />;
+		} else if (wishlistState.error) {
+			return <Alert>{JSON.stringify(wishlistState.error)}</Alert>;
+		} else if (wishlistState.items && wishlistState.items.length > 0) {
+			return <BookList books={wishlistState.items} />;
+		} else {
+			return (
+				<Alert>
+					no items added yet. see
+					<Link to='/books' className='alert-link mx-1'>
+						books
+					</Link>
+					or
+					<Link to='/category' className='alert-link mx-1'>
+						category
+					</Link>
+				</Alert>
+			);
+		}
+	};
 
-  const RenderWishlist = () => {
-    if (loading) {
-      return <RowOfPlaceholderCard num={6} />;
-    } else if (loadingError) {
-      return <Alert>{loadingError}</Alert>;
-    } else if (wishlistItems && wishlistItems.length > 0) {
-      return <BookList books={wishlistItems} />;
-    } else {
-      return (
-        <Alert>
-          no items added yet. see
-          <Link to='/books' className='alert-link mx-1'>
-            books
-          </Link>
-          or
-          <Link to='/category' className='alert-link mx-1'>
-            category
-          </Link>
-        </Alert>
-      );
-    }
-  };
-
-  return (
-    <>
-      <Banner title='wishlist' subtitle='favorites' />
-      <div className='container my-5 py-5'>
-        <RenderWishlist />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Banner title='wishlist' subtitle='favorites' />
+			<div className='container my-5 py-5'>
+				<RenderWishlist />
+			</div>
+		</>
+	);
 }
 
 export default Wishlist;
